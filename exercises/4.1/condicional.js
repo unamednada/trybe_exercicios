47cdd930afce120a95a570fe04a4cba4f06a7ea0
unamednada <dias.gustavo.dias@gmail.com>
2021-06-23 15:28:26 -0300

    Finish parte 4

---
+++ exercises/4.1/condicional.js
@@ -0,0 +1,3 @@
+let nota = 79;
+
+nota >= 80 ? console.log("Parabéns, você foi aprovada(o)!") : nota < 80 && nota >= 60 ? console.log("Você está na nossa lista de espera") : console.log("Você foi reprovada(o)");
