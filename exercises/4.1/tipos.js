4317e354c4566828f210ead1644e01ce85eb3c2e
unamednada <dias.gustavo.dias@gmail.com>
2021-06-23 15:22:34 -0300

    Finish exercise parte 3

--- exercises/4.1/tipos.js
+++ exercises/4.1/tipos.js
@@ -1,7 +1,9 @@
-let patientId = 50;
-let isEnrolled = true;
-const patientInfo = {
-  firstName: 'Ana',
-  lastName: 'Santos',
-};
-const patientEmail = 'ana@email.com';
+let base = 5;
+let altura = 8;
+let area = base * altura;
+
+console.log(base, altura, area);
+
+let perimetro = base * 2 + altura * 2;
+
+console.log(perimetro);
