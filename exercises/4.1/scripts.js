a39e15714fae3cb866dabede40768017454338a1
unamednada <dias.gustavo.dias@gmail.com>
2021-06-23 15:12:50 -0300

    Finish exercise 1

--- exercises/4.1/scripts.js
+++ exercises/4.1/scripts.js
@@ -3,3 +3,11 @@ const birthCity = "Nova Iguaçu";
 let birthYear = 1991;
 
 console.log(name, birthCity, birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+birthCity = "Rio";
+
+console.log(birthCity);
