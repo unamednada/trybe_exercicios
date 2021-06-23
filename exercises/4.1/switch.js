bb7609f5b986ad3b3c99e8f7a9c6fea278de1bee
unamednada <dias.gustavo.dias@gmail.com>
2021-06-23 15:33:11 -0300

    Finish parte 5

---
+++ exercises/4.1/switch.js
@@ -0,0 +1,15 @@
+let estado = "aprovada";
+
+switch (estado) {
+  case "aprovada":
+    console.log("Aprovada");
+    break;
+  case "lista":
+    console.log("Lista");
+    break;
+  case "reprovada":
+    console.log("Reprovada");
+    break;
+  default:
+    console.log("Não se aplica");
+}
