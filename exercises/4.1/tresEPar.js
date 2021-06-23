bf4f5468dd9acc0c963866979a47aa2bb67a2b49
unamednada <dias.gustavo.dias@gmail.com>
2021-06-23 18:48:04 -0300

    Fix tresEPar.js to detect even number

--- exercises/4.1/tresEPar.js
+++ exercises/4.1/tresEPar.js
@@ -6,7 +6,7 @@ n1 = parseInt(process.argv[2]);
 n2 = parseInt(process.argv[3]);
 n3 = parseInt(process.argv[4]);
 
-if (n1 % 2 != 0 || n2 % 2 != 0 || n3 % 2 != 0) {
+if (n1 % 2 == 0 || n2 % 2 == 0 || n3 % 2 == 0) {
   console.log("true");
 } else {
   console.log("false");
