b0524172602b304f56644b2da38a74bedc682715
unamednada <dias.gustavo.dias@gmail.com>
2021-06-24 14:58:02 -0300

    Finish for

---
+++ exercises/4.2/for.js
@@ -0,0 +1,9 @@
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+groceryList.forEach((value, index) => console.log(index + 1 + ": " + value));
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (name of names) {
+  console.log(name);
+}
