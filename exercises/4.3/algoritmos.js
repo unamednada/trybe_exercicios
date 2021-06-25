00df51efa7f571169465d388cc6509c17c42b454
unamednada <dias.gustavo.dias@gmail.com>
2021-06-25 18:03:41 -0300

    Finish primoOuNao

--- exercises/4.3/algoritmos.js
+++ exercises/4.3/algoritmos.js
@@ -150,4 +150,22 @@ const piramideVazia = n => {
   }
 }
 
-piramideVazia(11);
+// piramideVazia(10);
+
+const primoOuNao = n => {
+  let divisoes = 0;
+
+  for (let i = 2; i < n; i += 1) {
+    if (n % i == 0) {
+      divisoes += 1;
+    }
+  }
+
+  if (divisoes == 0) {
+    console.log("O número " + n + " é primo!");
+  } else {
+    console.log("O número " + n + " não é primo.");
+  }
+}
+
+primoOuNao(7);
