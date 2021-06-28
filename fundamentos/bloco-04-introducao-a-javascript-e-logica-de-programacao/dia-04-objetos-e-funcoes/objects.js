daa9ab9c2cdeea996010f49748832a0659871a9e
unamednada <dias.gustavo.dias@gmail.com>
2021-06-28 18:28:11 -0300

    Create get mensagem() for info in objects.js

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/objects.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/objects.js
@@ -2,4 +2,10 @@ let info = {
   personagem: 'Margarida',
   origem: 'Pato Donald',
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-};
+  get mensagem() {
+    return `Bem-vinda, ${this.personagem}`;
+  }
+}
+
+console.log(info.mensagem);
+
