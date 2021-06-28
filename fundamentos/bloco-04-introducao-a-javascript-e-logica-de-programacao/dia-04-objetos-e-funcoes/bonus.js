cce8fa9a32450a8628181b6f46ccff33b257b6f6
unamednada <dias.gustavo.dias@gmail.com>
2021-06-28 19:27:35 -0300

    Start outline of romanosParaArabes in bonus.js

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/bonus.js
@@ -0,0 +1,18 @@
+let valoresRomanos = {
+  i: 1,
+  v: 5,
+  x: 10,
+  l: 50,
+  c: 100,
+  d: 500,
+  m: 1000
+}
+
+const romanosParaArabes = romanoStr => {
+  let arabe = 0;
+  for (let romano of romanoStr) {
+    for (let chave in valoresRomanos) {
+      if 
+    }
+  }
+}
