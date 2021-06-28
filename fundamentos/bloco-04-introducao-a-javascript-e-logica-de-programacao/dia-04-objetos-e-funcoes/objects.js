744ad09310f03b2caac5a39dcba650761a675292
unamednada <dias.gustavo.dias@gmail.com>
2021-06-28 18:38:14 -0300

    Create second object in objects.js

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/objects.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/objects.js
@@ -13,10 +13,24 @@ info.recorrente = 'Sim';
 
 // console.log(info);
 
-for (let propriedade in info) {
-  console.log(propriedade);
+// for (let propriedade in info) {
+//   console.log(propriedade);
+// }
+
+// for (let propriedade in info) {
+//   console.log(info[propriedade]);
+// }
+
+let secondInfo = {
+  personagem: 'Tio Patinhas',
+  origem: 'Christmas on Bear Mountain, Dell\'s Four Color Comics #178',
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim'
 }
 
-for (let propriedade in info) {
-  console.log(info[propriedade]);
+console.log(`${info.personagem} e ${secondInfo.personagem}`);
+console.log(`${info.origem} e ${secondInfo.origem}`);
+console.log(`${info.nota} e ${secondInfo.nota}`);
+if (info.recorrente === 'Sim' && secondInfo.recorrente === 'Sim') {
+  console.log("Ambos recorrentes");
 }
