e224e5c0f03e477bb5fe191e9f641cae9a228607
unamednada <dias.gustavo.dias@gmail.com>
2021-06-28 18:49:18 -0300

    Create indexMenorValor in functions.js

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/functions.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/functions.js
@@ -24,4 +24,19 @@ const indexMaiorValor = array => {
   return maxIndex;
 }
 
-console.log(indexMaiorValor([2, 3, 6, 7, 10, 1]));
+// console.log(indexMaiorValor([2, 3, 6, 7, 10, 1]));
+
+const indexMenorValor = array => {
+  let min = array[0];
+  let minIndex = 0;
+  for (index in array) {
+    if (array[index] < min) {
+      min = array[index];
+      minIndex = index;
+    }
+  }
+
+  return minIndex;
+}
+
+console.log(indexMenorValor([2, 4, 6, 7, 10, 0, -3]));
