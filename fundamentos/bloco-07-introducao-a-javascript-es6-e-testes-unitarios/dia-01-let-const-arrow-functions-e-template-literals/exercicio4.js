0e230baa6a9fa458471e5e090c98d6aaf721168e
unamednada <dias.gustavo.dias@gmail.com>
2021-07-23 10:35:15 -0300

    Create returnSKills

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-let-const-arrow-functions-e-template-literals/exercicio4.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-let-const-arrow-functions-e-template-literals/exercicio4.js
@@ -1,3 +1,15 @@
 const addToString = (str, word) =>  str.replace('x', word);
 
 console.log(addToString('Tryber x aqui!', 'Bebeto'));
+
+const mySkills = ['Webdev', 'Communication', 'Resilience', 'Data analysis', 'Project management'];
+
+const returnSkills = () => {
+  mySkills.sort();
+  console.log(mySkills);
+  return `${addToString('Tryber x aqui!', 'Gustavo')}! Minhas cinco principais habilidades são:
+  ${mySkills.join(';\n  ')};
+  #goTrybe`;
+}
+
+console.log(returnSkills());
