14f55c64595357b7ab64bfaa043d14e1ea996187
unamednada <dias.gustavo.dias@gmail.com>
2021-07-26 19:11:53 -0300

    Copy greaterthanten to tdd2.js

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitarios-em-javascript/tdd2.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitarios-em-javascript/tdd2.js
@@ -13,22 +13,35 @@ const assert = require('assert');
 
 // // console.log(typeof(result));
 
-const removeVowels = (word) => {
-  let charArray = word.split('');
+// const removeVowels = (word) => {
+//   let charArray = word.split('');
   
-  let vowelCount = 1;
-  for (let i = 0; i < charArray.length; i += 1) {
-    if (['a', 'e', 'i', 'o', 'u'].includes(charArray[i])) {
-    charArray[i] = vowelCount.toString();
-    vowelCount += 1;
+//   let vowelCount = 1;
+//   for (let i = 0; i < charArray.length; i += 1) {
+//     if (['a', 'e', 'i', 'o', 'u'].includes(charArray[i])) {
+//     charArray[i] = vowelCount.toString();
+//     vowelCount += 1;
+//     }
+//   }
+//   return charArray.join('');
+// }
+
+// const parameter = 'Dayane';
+// const result = 'D1y2n3';
+
+// assert.strictEqual(typeof(removeVowels), 'function');
+// assert.strictEqual(typeof(removeVowels(parameter)), 'string');
+// assert.strictEqual(removeVowels(parameter), result);
+
+const greaterThanTen = (array) => {
+  let results = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] > 10) {
+      results += array[index];
     }
   }
-  return charArray.join('');
-}
+  return results;
+};
 
-const parameter = 'Dayane';
-const result = 'D1y2n3';
-
-assert.strictEqual(typeof(removeVowels), 'function');
-assert.strictEqual(typeof(removeVowels(parameter)), 'string');
-assert.strictEqual(removeVowels(parameter), result);
+const parameter = [4, 10, 32, 9, 21];
+const result = [32, 21];
