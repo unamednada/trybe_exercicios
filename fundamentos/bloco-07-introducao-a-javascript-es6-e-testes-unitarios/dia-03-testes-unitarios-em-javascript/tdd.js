1712e34a4dd2b8183bb6a653b531be9cd53d2c73
unamednada <dias.gustavo.dias@gmail.com>
2021-07-26 18:33:06 -0300

    Copy wordLengths code from course

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitarios-em-javascript/tdd.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitarios-em-javascript/tdd.js
@@ -1,13 +1,23 @@
+// const assert = require('assert');
+
+// const addOne = array => array.map((value) => value + 1); 
+
+// const myArray = [31, 57, 12, 5];
+// const unchanged = [31, 57, 12, 5];
+// const expected = [32, 58, 13, 6];
+// const output = addOne(myArray);
+
+
+// assert.strictEqual(typeof addOne, 'function');
+// assert.deepStrictEqual(output, expected);
+// assert.deepStrictEqual(myArray, unchanged);
+
 const assert = require('assert');
+// escreva a função wordLengths aqui
 
-const addOne = array => array.map((value) => value + 1); 
+const words = ['sun', 'potato', 'roundabout', 'pizza'];
+const expected = [3, 6, 10, 5];
 
-const myArray = [31, 57, 12, 5];
-const unchanged = [31, 57, 12, 5];
-const expected = [32, 58, 13, 6];
-const output = addOne(myArray);
-
-
-assert.strictEqual(typeof addOne, 'function');
+assert.strictEqual(typeof wordLengths, 'function');
+const output = wordLengths(words);
 assert.deepStrictEqual(output, expected);
-assert.deepStrictEqual(myArray, unchanged);
