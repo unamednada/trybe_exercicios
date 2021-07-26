659b31613c69e44b1ef2e9b1f131e0cee18c4d0c
unamednada <dias.gustavo.dias@gmail.com>
2021-07-26 19:18:12 -0300

    Refactor greaterthanten to pass test. Success

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitarios-em-javascript/tdd2.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitarios-em-javascript/tdd2.js
@@ -34,15 +34,7 @@ const { type } = require('os');
 // assert.strictEqual(typeof(removeVowels(parameter)), 'string');
 // assert.strictEqual(removeVowels(parameter), result);
 
-const greaterThanTen = (array) => {
-  let results = 0;
-  for (let index = 0; index < array.length; index += 1) {
-    if (array[index] > 10) {
-      results += array[index];
-    }
-  }
-  return results;
-};
+const greaterThanTen = array => array.filter((number) => number > 10);
 
 const parameter = [4, 10, 32, 9, 21];
 const result = [32, 21];
