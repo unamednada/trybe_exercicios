d70601e409540208c687552fe9a17096bc005bb4
unamednada <dias.gustavo.dias@gmail.com>
2021-07-26 19:11:12 -0300

    Refactor removevowel to pass test. Sucess

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitarios-em-javascript/tdd2.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitarios-em-javascript/tdd2.js
@@ -14,17 +14,21 @@ const assert = require('assert');
 // // console.log(typeof(result));
 
 const removeVowels = (word) => {
+  let charArray = word.split('');
+  
   let vowelCount = 1;
-  for (let i = 0; i < word.length; i += 1) {
-    if (['a', 'e', 'i', 'o', 'u'].includes(word[i])) {
-      word[i] = vowelCount;
-      vowelCount += 1;
+  for (let i = 0; i < charArray.length; i += 1) {
+    if (['a', 'e', 'i', 'o', 'u'].includes(charArray[i])) {
+    charArray[i] = vowelCount.toString();
+    vowelCount += 1;
     }
   }
-  return word;
+  return charArray.join('');
 }
+
 const parameter = 'Dayane';
 const result = 'D1y2n3';
 
 assert.strictEqual(typeof(removeVowels), 'function');
 assert.strictEqual(typeof(removeVowels(parameter)), 'string');
+assert.strictEqual(removeVowels(parameter), result);
