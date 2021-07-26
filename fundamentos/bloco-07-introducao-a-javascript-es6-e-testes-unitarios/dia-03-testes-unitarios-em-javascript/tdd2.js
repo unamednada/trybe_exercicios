5ae65f0c18fc7e250bea2ddd20d0278282fb3dfc
unamednada <dias.gustavo.dias@gmail.com>
2021-07-26 18:48:27 -0300

    Copy removeVowels from course

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitarios-em-javascript/tdd2.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitarios-em-javascript/tdd2.js
@@ -1,15 +1,38 @@
-const assert = require('assert');
+// const assert = require('assert');
 
-const greetPeople = (people) => parameter.map((person) => 'Hello ' + person);
+// const greetPeople = (people) => parameter.map((person) => 'Hello ' + person);
 
-const parameter = ['Irina', 'Ashleigh', 'Elsa'];
-const result = ['Hello Irina', 'Hello Ashleigh', 'Hello Elsa'];
+// const parameter = ['Irina', 'Ashleigh', 'Elsa'];
+// const result = ['Hello Irina', 'Hello Ashleigh', 'Hello Elsa'];
 
-// console.log(greetPeople(parameter));
+// // console.log(greetPeople(parameter));
 
-assert.strictEqual(typeof(greetPeople), 'function');
-assert.strictEqual(typeof(greetPeople(parameter)), 'object');
-assert.deepStrictEqual(greetPeople(parameter), result);
+// assert.strictEqual(typeof(greetPeople), 'function');
+// assert.strictEqual(typeof(greetPeople(parameter)), 'object');
+// assert.deepStrictEqual(greetPeople(parameter), result);
 
-// console.log(typeof(result));
+// // console.log(typeof(result));
 
+const removeVowels = (word) => {
+  const characters = word.split('');
+  const results = [];
+
+  for (let index = 0; index < characters.length; index += 1) {
+    if (
+      characters[index] === 'a' ||
+      characters[index] === 'o' ||
+      characters[index] === 'i' ||
+      characters[index] === 'e' ||
+      characters[index] === 'u'
+    ) {
+      results.push(characters[index]);
+    } else {
+      results.push('_');
+    }
+  }
+  return results;
+};
+
+
+const parameter = 'Dayane';
+const result = 'D1y2n3';
