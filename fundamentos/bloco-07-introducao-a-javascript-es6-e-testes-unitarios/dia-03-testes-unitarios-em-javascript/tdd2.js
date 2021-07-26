17f1aa05e18bc39ee2e6867af3a898ad57eb26a9
unamednada <dias.gustavo.dias@gmail.com>
2021-07-26 19:21:00 -0300

    Test typeof secondthirdsmallest. Success

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitarios-em-javascript/tdd2.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitarios-em-javascript/tdd2.js
@@ -54,3 +54,5 @@ function secondThirdSmallest(array) {
 
 const parameter = [4, 10, 32, 9, 21, 90, 5, 11, 8, 6];
 const result = [5, 6];
+
+assert.strictEqual(typeof(secondThirdSmallest(parameter)), 'object');
