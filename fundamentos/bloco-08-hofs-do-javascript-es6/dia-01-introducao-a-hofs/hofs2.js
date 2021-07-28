3b9cfe68bf0e0a621983ef2c1792e4279dbc8dae
unamednada <dias.gustavo.dias@gmail.com>
2021-07-28 20:26:56 -0300

    Create onetoten test successful return string

--- fundamentos/bloco-08-hofs-do-javascript-es6/dia-01-introducao-a-hofs/hofs2.js
+++ fundamentos/bloco-08-hofs-do-javascript-es6/dia-01-introducao-a-hofs/hofs2.js
@@ -1,6 +1,10 @@
 const assert = require('assert');
+const { type } = require('os');
+
+const oneToTen = n => n === Math.floor(Math.random() * 10);
 
 const casino = (number, game) => game(number) ? 'Parabéns, você ganhou!' : 'Tente novamente';
 
 assert.strictEqual(typeof casino, 'function');
 assert.strictEqual(typeof casino(3, (n) => n < 0 ), 'string');
+assert.strictEqual(typeof casino(3, oneToTen), 'string');
