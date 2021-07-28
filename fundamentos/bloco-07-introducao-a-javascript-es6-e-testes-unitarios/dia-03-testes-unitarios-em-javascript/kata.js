f155fd20105dbeeb68782f7f8de8f414f3086ff0
unamednada <dias.gustavo.dias@gmail.com>
2021-07-28 15:42:31 -0300

    Refactor verify numbers success, change test to return false in all characters

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitarios-em-javascript/kata.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitarios-em-javascript/kata.js
@@ -9,6 +9,8 @@ class PasswordVerifier {
     if (upperCase.length === 0) return false;
     const lowerCase = password.split('').filter((letter) => letter.toLowerCase() === letter);
     if (lowerCase.length === 0) return false;
+    const numbers = password.split('').filter((letter) => typeof letter === 'number');
+    if (numbers.length === 0) return false;
     return true;
   }
 
@@ -21,6 +23,6 @@ assert.strictEqual(typeof verify, 'object');
 assert.strictEqual(verify.Verify(), false);
 assert.strictEqual(verify.Verify('123'), false);
 assert.strictEqual(verify.Verify('aaaaaaaaa'), false);
-assert.strictEqual(verify.Verify('Aaaaaaaaa'), true);
+assert.strictEqual(verify.Verify('Aaaaaaaaa'), false);
 assert.strictEqual(verify.Verify('AAAAAAAAA'), false);
 assert.strictEqual(verify.Verify('aaaaaAaaAa'), false);
