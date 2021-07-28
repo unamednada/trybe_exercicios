5ac0cb4ecc2ae282301e5805622b8b7ecd9abdad
unamednada <dias.gustavo.dias@gmail.com>
2021-07-28 20:12:05 -0300

    Create test for id1 typeof success

--- fundamentos/bloco-08-hofs-do-javascript-es6/dia-01-introducao-a-hofs/hofs.js
+++ fundamentos/bloco-08-hofs-do-javascript-es6/dia-01-introducao-a-hofs/hofs.js
@@ -1,22 +1,26 @@
 const assert = require('assert');
-const { create } = require('domain');
 
 const createEmployee = (fullName) => {
   const nameArray = fullName.split(' ');
   const email = `${nameArray[0].toLowerCase()}_${nameArray[1].toLowerCase()}@trybe.com`
   return { fullName: fullName, email: email};
-}
-const newEmployees = () => {
-  const employees = {
-    id1: '', // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+};
+
+const newEmployees = (callBack) => {
+  return {
+    id1: callBack('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
     id2: '', // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
     id3: '', // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
   }
-  return employees;
 };
 
+const myCompanyStaff = newEmployees(createEmployee);
+console.log(myCompanyStaff.id1);
+
+assert.strictEqual(typeof myCompanyStaff.id1, 'object');
+
 assert.strictEqual(typeof newEmployees, 'function');
-assert.strictEqual(typeof newEmployees(), 'object');
+assert.strictEqual(typeof newEmployees(createEmployee), 'object');
 assert.strictEqual(typeof createEmployee, 'function');
 assert.strictEqual(typeof createEmployee('Gustavo Dias'), 'object');
 assert.deepStrictEqual(createEmployee('Gustavo Dias'), { fullName: 'Gustavo Dias', email: 'gustavo_dias@trybe.com'});
