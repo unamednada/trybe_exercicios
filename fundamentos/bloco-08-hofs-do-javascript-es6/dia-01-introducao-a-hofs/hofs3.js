50bd40f0a6c1261ef47dcaeb720b85eb224e8a4b
unamednada <dias.gustavo.dias@gmail.com>
2021-07-28 20:37:58 -0300

    Refactor totalpoints to take checkanswers as parameter test result number successful

--- fundamentos/bloco-08-hofs-do-javascript-es6/dia-01-introducao-a-hofs/hofs3.js
+++ fundamentos/bloco-08-hofs-do-javascript-es6/dia-01-introducao-a-hofs/hofs3.js
@@ -3,9 +3,11 @@ const assert = require('assert');
 const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
 const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
 
-const checkAnswers = () => {};
+const checkAnswers = () => 0;
 
-const totalPoints = () => {};
+const totalPoints = (checker) => checker();
 
 assert.strictEqual(typeof totalPoints, 'function');
 assert.strictEqual(typeof checkAnswers, 'function');
+assert.strictEqual(typeof checkAnswers(), 'number');
+assert.strictEqual(typeof totalPoints(checkAnswers), 'number');
