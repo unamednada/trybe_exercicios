8bfcc413b4ec2795d70f8c50bd51932265223595
unamednada <dias.gustavo.dias@gmail.com>
2021-07-28 20:32:28 -0300

    Copy answers to hofs3.js

--- fundamentos/bloco-08-hofs-do-javascript-es6/dia-01-introducao-a-hofs/hofs3.js
+++ fundamentos/bloco-08-hofs-do-javascript-es6/dia-01-introducao-a-hofs/hofs3.js
@@ -1,5 +1,8 @@
 const assert = require('assert');
 
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
 const checkAnswers = () => {};
 
 const totalPoints = () => {};
