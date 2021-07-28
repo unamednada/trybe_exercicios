33330a5a1883981bf044aefe75d6d6b5ae8b6008
unamednada <dias.gustavo.dias@gmail.com>
2021-07-28 15:34:18 -0300

    Test verify all lowercase fail

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitarios-em-javascript/kata.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitarios-em-javascript/kata.js
@@ -5,6 +5,8 @@ class PasswordVerifier {
   Verify(password) {
     if (!password) return false;
     if (password.length <= 8) return false;
+    // const upperCase = password.split('').filter((letter) => letter.toUpperCase() === letter);
+    // if (upperCase.length === 0) return false;
     return true;
   }
 
@@ -16,3 +18,4 @@ assert.strictEqual(typeof PasswordVerifier, 'function');
 assert.strictEqual(typeof verify, 'object');
 assert.strictEqual(verify.Verify(), false);
 assert.strictEqual(verify.Verify('123'), false);
+assert.strictEqual(verify.Verify('aaaaaaaaa'), false);
