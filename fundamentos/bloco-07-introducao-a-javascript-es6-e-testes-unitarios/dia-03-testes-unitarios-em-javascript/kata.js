7ceaea31fa21d9b0397740b97af335a4856ec2d8
unamednada <dias.gustavo.dias@gmail.com>
2021-07-28 15:28:55 -0300

    Change test empty verify to false, fail test verify 123 true

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitarios-em-javascript/kata.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitarios-em-javascript/kata.js
@@ -2,8 +2,8 @@ const assert = require('assert');
 
 class PasswordVerifier {
 
-  Verify() {
-    return true;
+  Verify(password) {
+    return password ? password.length > 8 : false;
   }
 
 };
@@ -12,4 +12,5 @@ const verify = new PasswordVerifier();
 
 assert.strictEqual(typeof PasswordVerifier, 'function');
 assert.strictEqual(typeof verify, 'object');
-assert.strictEqual(verify.Verify(), true);
+assert.strictEqual(verify.Verify(), false);
+assert.strictEqual(verify.Verify('123'), true);
