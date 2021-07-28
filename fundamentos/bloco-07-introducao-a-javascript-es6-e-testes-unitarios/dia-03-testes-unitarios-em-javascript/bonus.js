449707a3cc3187718ce33ea8a1ea0346fdc04936
unamednada <dias.gustavo.dias@gmail.com>
2021-07-28 14:40:20 -0300

    Copy getchange to bonus.js

---
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitarios-em-javascript/bonus.js
@@ -0,0 +1,10 @@
+function getChange(payable, paid) {
+  const coins = [200, 100, 50, 20, 10, 5, 2, 1];
+  const change = [];
+  const { length } = coins;
+  let remaining = paid - payable;
+
+  // escreva seu código aqui...
+
+  return change;
+}
