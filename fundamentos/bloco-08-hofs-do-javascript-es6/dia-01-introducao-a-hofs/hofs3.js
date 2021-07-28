a87ef000ec3f3ff51463271fe8b0a5a1bee04be2
unamednada <dias.gustavo.dias@gmail.com>
2021-07-28 20:47:41 -0300

    refactor all functions to pass final test. Success

--- fundamentos/bloco-08-hofs-do-javascript-es6/dia-01-introducao-a-hofs/hofs3.js
+++ fundamentos/bloco-08-hofs-do-javascript-es6/dia-01-introducao-a-hofs/hofs3.js
@@ -3,11 +3,11 @@ const assert = require('assert');
 const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
 const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
 
-const checkAnswers = () => 0;
+const checkAnswers = (ssAnswers, keyAnswers) => ssAnswers.map((answer, index) => answer === keyAnswers[index] ? 1 : -0.5).reduce((sum, current) => sum + current, 0);
 
-const totalPoints = (checker) => checker();
+const totalPoints = (students, key, checker) => checker(students, key);
 
 assert.strictEqual(typeof totalPoints, 'function');
 assert.strictEqual(typeof checkAnswers, 'function');
-assert.strictEqual(typeof checkAnswers(), 'number');
-assert.strictEqual(typeof totalPoints(checkAnswers), 'number');
+assert.strictEqual(typeof checkAnswers(STUDENT_ANSWERS, RIGHT_ANSWERS), 'number');
+assert.strictEqual(typeof totalPoints(STUDENT_ANSWERS, RIGHT_ANSWERS, checkAnswers), 'number');
