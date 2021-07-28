94e0f5d504623110c231f089c07381c8c926ab75
unamednada <dias.gustavo.dias@gmail.com>
2021-07-28 20:25:24 -0300

    Change expected to string success

--- fundamentos/bloco-08-hofs-do-javascript-es6/dia-01-introducao-a-hofs/hofs2.js
+++ fundamentos/bloco-08-hofs-do-javascript-es6/dia-01-introducao-a-hofs/hofs2.js
@@ -3,4 +3,4 @@ const assert = require('assert');
 const casino = (number, game) => game(number) ? 'Parabéns, você ganhou!' : 'Tente novamente';
 
 assert.strictEqual(typeof casino, 'function');
-assert.strictEqual(typeof casino(3, (n) => n < 0 ), 'number');
+assert.strictEqual(typeof casino(3, (n) => n < 0 ), 'string');
