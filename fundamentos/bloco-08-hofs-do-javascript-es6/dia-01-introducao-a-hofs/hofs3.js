564f4059e32df0d71f8d7518617f0276d6117f33
unamednada <dias.gustavo.dias@gmail.com>
2021-07-28 20:31:58 -0300

    test, fail and create totalpoints typeof function success

--- fundamentos/bloco-08-hofs-do-javascript-es6/dia-01-introducao-a-hofs/hofs3.js
+++ fundamentos/bloco-08-hofs-do-javascript-es6/dia-01-introducao-a-hofs/hofs3.js
@@ -1,5 +1,8 @@
 const assert = require('assert');
 
+const checkAnswers = () => {};
+
 const totalPoints = () => {};
 
 assert.strictEqual(typeof totalPoints, 'function');
+assert.strictEqual(typeof checkAnswers, 'function');
