ad9d55deba5942425c32d19e3ea927a96f17d1a4
unamednada <dias.gustavo.dias@gmail.com>
2021-07-28 20:23:21 -0300

    Change test to function. Success

--- fundamentos/bloco-08-hofs-do-javascript-es6/dia-01-introducao-a-hofs/hofs2.js
+++ fundamentos/bloco-08-hofs-do-javascript-es6/dia-01-introducao-a-hofs/hofs2.js
@@ -2,4 +2,4 @@ const assert = require('assert');
 
 const casino = (number, game) => game(number) ? 'Parabéns, você ganhou!' : 'Tente novamente';
 
-assert.strictEqual(typeof casino, 'object');
+assert.strictEqual(typeof casino, 'function');
