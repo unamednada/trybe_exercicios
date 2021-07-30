1d6238a2bfd27cadfff559d0121a62ba7970176c
unamednada <dias.gustavo.dias@gmail.com>
2021-07-30 18:50:07 -0300

    Create oldbooksordered

--- fundamentos/bloco-08-hofs-do-javascript-es6/dia-03-hofs-map-e-filter/exercise1.js
+++ fundamentos/bloco-08-hofs-do-javascript-es6/dia-03-hofs-map-e-filter/exercise1.js
@@ -141,3 +141,31 @@ const expectedResult3 = [
 const fantasyOrScienceFiction = () => books.filter((book) => book.genre === 'Ficção Científica' || book.genre === 'Fantasia');
 
 assert.deepStrictEqual(fantasyOrScienceFiction(), expectedResult3);
+
+const expectedResult4 = [
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+    releaseYear: 1928,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: { name: 'Isaac Asimov', birthYear: 1920 },
+    releaseYear: 1951,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954,
+  },
+];
+
+const oldBooksOrdered = () => books.filter((book) => 2020 - book.releaseYear > 60).sort((a, b) => a.releaseYear - b.releaseYear);
+
+assert.deepStrictEqual(oldBooksOrdered(), expectedResult4);
