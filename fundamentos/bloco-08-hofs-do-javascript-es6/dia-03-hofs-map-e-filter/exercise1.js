00b3af2f37757d0de74479277ab4d943c80bb3b3
unamednada <dias.gustavo.dias@gmail.com>
2021-07-30 18:26:16 -0300

    Create formatedbooknames

--- fundamentos/bloco-08-hofs-do-javascript-es6/dia-03-hofs-map-e-filter/exercise1.js
+++ fundamentos/bloco-08-hofs-do-javascript-es6/dia-03-hofs-map-e-filter/exercise1.js
@@ -63,4 +63,15 @@ const books = [
   },
 ];
 
-// Adicione o código do exercício aqui:
+const expectedResult1 = [
+  'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
+  'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
+  'Fundação - Ficção Científica - Isaac Asimov',
+  'Duna - Ficção Científica - Frank Herbert',
+  'A Coisa - Terror - Stephen King',
+  'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
+];
+
+const formatedBookNames = () =>  books.map((book) => `${book.name} - ${book.genre} - ${book.author.name}`);
+
+assert.deepStrictEqual(formatedBookNames(), expectedResult1);
