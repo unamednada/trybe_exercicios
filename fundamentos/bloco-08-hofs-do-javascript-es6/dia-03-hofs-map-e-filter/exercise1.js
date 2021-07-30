076ddbee68a7a196be4a8f7e9000ae44d867e2e6
unamednada <dias.gustavo.dias@gmail.com>
2021-07-30 18:32:00 -0300

    Create name and age]

--- fundamentos/bloco-08-hofs-do-javascript-es6/dia-03-hofs-map-e-filter/exercise1.js
+++ fundamentos/bloco-08-hofs-do-javascript-es6/dia-03-hofs-map-e-filter/exercise1.js
@@ -75,3 +75,34 @@ const expectedResult1 = [
 const formatedBookNames = () =>  books.map((book) => `${book.name} - ${book.genre} - ${book.author.name}`);
 
 assert.deepStrictEqual(formatedBookNames(), expectedResult1);
+
+const expectedResult2 = [
+  {
+    age: 31,
+    author: 'Isaac Asimov',
+  },
+  {
+    age: 38,
+    author: 'H. P. Lovecraft',
+  },
+  {
+    age: 39,
+    author: 'Stephen King',
+  },
+  {
+    age: 43,
+    author: 'George R. R. Martin',
+  },
+  {
+    age: 45,
+    author: 'Frank Herbert',
+  },
+  {
+    age: 62,
+    author: 'J. R. R. Tolkien',
+  },
+];
+
+const nameAndAge = () => books.map((book) => ({ age: book.releaseYear - book.author.birthYear, author: book.author.name })).sort((a, b) => a.age - b.age);
+
+assert.deepStrictEqual(nameAndAge(), expectedResult2);
