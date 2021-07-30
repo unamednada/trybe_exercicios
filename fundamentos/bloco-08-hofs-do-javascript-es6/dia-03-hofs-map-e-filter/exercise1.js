dacaa79dd46e0fd7dd88a09c2c8348ee0e2050f8
unamednada <dias.gustavo.dias@gmail.com>
2021-07-30 18:34:46 -0300

    Create fantasyorscifi

--- fundamentos/bloco-08-hofs-do-javascript-es6/dia-03-hofs-map-e-filter/exercise1.js
+++ fundamentos/bloco-08-hofs-do-javascript-es6/dia-03-hofs-map-e-filter/exercise1.js
@@ -106,3 +106,38 @@ const expectedResult2 = [
 const nameAndAge = () => books.map((book) => ({ age: book.releaseYear - book.author.birthYear, author: book.author.name })).sort((a, b) => a.age - b.age);
 
 assert.deepStrictEqual(nameAndAge(), expectedResult2);
+
+const expectedResult3 = [
+  { 
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: { name: 'George R. R. Martin', birthYear: 1948 },
+    releaseYear: 1991
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: { name: 'Isaac Asimov', birthYear: 1920 },
+    releaseYear: 1951
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: { name: 'Frank Herbert', birthYear: 1920 },
+    releaseYear: 1965
+  }
+];
+
+const fantasyOrScienceFiction = () => books.filter((book) => book.genre === 'Ficção Científica' || book.genre === 'Fantasia');
+
+assert.deepStrictEqual(fantasyOrScienceFiction(), expectedResult3);
