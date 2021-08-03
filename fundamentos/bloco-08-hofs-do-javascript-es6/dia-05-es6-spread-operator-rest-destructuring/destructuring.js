25afb8acf75bac28f2eb01858006ed4eac952167
unamednada <dias.gustavo.dias@gmail.com>
2021-08-03 14:58:05 -0300

    Create toObject

--- fundamentos/bloco-08-hofs-do-javascript-es6/dia-05-es6-spread-operator-rest-destructuring/destructuring.js
+++ fundamentos/bloco-08-hofs-do-javascript-es6/dia-05-es6-spread-operator-rest-destructuring/destructuring.js
@@ -69,3 +69,15 @@ const swappedList = swap(myList);
 assert.strictEqual(swappedList[0], 3);
 assert.strictEqual(swappedList[1], 2);
 assert.strictEqual(swappedList[2], 1);
+
+const palio = ['Palio', 'Fiat', 2019];
+const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
+const chiron = ['Chiron', 'Bugatti', 2016];
+
+// escreva toObject abaixo
+
+const toObject = ([name, brand, year]) => ({ name, brand, year});
+
+assert.deepStrictEqual(toObject(palio), { name: 'Palio', brand: 'Fiat', year: 2019 });
+assert.deepStrictEqual(toObject(shelbyCobra), { name: 'Shelby Cobra', brand: 'Ford', year: 1963 });
+assert.deepStrictEqual(toObject(chiron), { name: 'Chiron', brand: 'Bugatti', year: 2016 });
