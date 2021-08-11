021a7991e153f00102eab9bce74f376177d03736
unamednada <dias.gustavo.dias@gmail.com>
2021-08-11 15:29:33 -0300

    Use response.ok to finish async await fetchjoke in bonusScript.js

--- fundamentos/bloco-09-javascript-assincrono-e-promises/dia-02-javascript-promises/bonusScript.js
+++ fundamentos/bloco-09-javascript-assincrono-e-promises/dia-02-javascript-promises/bonusScript.js
@@ -1,16 +1,17 @@
 const API_URL = 'https://icanhazdadjoke.com/';
 
-const fetchJoke = async () => {
-  const params = {
-    method: 'GET',
-    headers: { 'Accept': 'application/json' },
-  };
+const jokeRequestObj = {
+  method: 'GET',
+  headers: { 'Accept': 'application/json' },
+};
 
-  fetch(API_URL, params)
-  .then((response) => response.json())
-  .then((jsonResponse) => {
-    document.getElementById('jokeContainer').innerText = jsonResponse.joke;
-  });
+const fetchJoke = async () => {
+    const response = await fetch(API_URL, jokeRequestObj);
+    // Learned at codecademy JavaScript course:
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      document.getElementById('jokeContainer').innerText = jsonResponse.joke;
+    }
 };
 
 window.onload = () => fetchJoke();
