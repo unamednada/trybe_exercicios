d43e184767949fca1beec2e28654518928a4f03c
unamednada <dias.gustavo.dias@gmail.com>
2021-08-17 12:57:38 -0300

    Create and pass first test for myfizzbuzz

---
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-01-primeiros-passos-no-jest/parte-1/myFizzBuzz.test.js
@@ -0,0 +1,9 @@
+const myFizzBuzz = require('./myFizzBuzz.js');
+
+describe('A função myFizzBuzz(num) retorna \"fizzbuzz\" se o número for divisível por 3 e 5, retorna \"fizz\" se o número for divisível apenas por 3, retorna \"buzz\" se divisível apenas por 5, retorna <num> se não for divisível por 3 ou 5 e <false> caso o parâmetro não seja um numero', () => {
+
+  it('myFizzBuzz() tem o retorno esperado quando passamos um número divisível por 3 e 5', () => {
+    expect('fizzbuzz').toBe(myFizzBuzz(15));
+  })
+
+})
