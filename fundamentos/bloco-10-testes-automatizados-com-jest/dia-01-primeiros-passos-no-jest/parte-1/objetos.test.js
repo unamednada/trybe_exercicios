3ff9dd8c35455939fecf968f0106ca1273297251
unamednada <dias.gustavo.dias@gmail.com>
2021-08-17 13:50:48 -0300

    Reword last test description and add source to objectcontaining test

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-01-primeiros-passos-no-jest/parte-1/objetos.test.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-01-primeiros-passos-no-jest/parte-1/objetos.test.js
@@ -8,12 +8,13 @@ describe('Compare 2 objetos para verificar se são idênticos ou não', () => {
   });
 
   it('Comparar as propriedades de 2 objetos para verificar se são idênticos', () => {
+    // fonte: https://codewithhugo.com/jest-array-object-match-contain/
     expect(obj1).toEqual(
       expect.objectContaining({...obj2})
     );
   });
 
-  it('Comparar o array de chaves e de valores de 2 objetos para verificar se são idênticos', () => {
+  it('Comparar o array de chaves e de valores de 2 objetos para verificar se são ESTRITAMENTE idênticos', () => {
     expect([Object.keys(obj1), Object.values(obj1)]).not.toEqual([Object.keys(obj3), Object.values(obj3)]);
   });
 
