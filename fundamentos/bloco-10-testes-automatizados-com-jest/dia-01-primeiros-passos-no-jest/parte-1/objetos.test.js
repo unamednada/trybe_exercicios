d83abb53a63e1bb0c652498c92ef0d056d6c66a9
unamednada <dias.gustavo.dias@gmail.com>
2021-08-17 13:33:59 -0300

    Fail test toequal obj2 obj3

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-01-primeiros-passos-no-jest/parte-1/objetos.test.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-01-primeiros-passos-no-jest/parte-1/objetos.test.js
@@ -3,8 +3,8 @@ const objetos = require('./objetos');
 describe('Compare 2 objetos para verificar se são idênticos ou não', () => {
 
   it('Comparar 2 objetos não idênticos', () => {
-    const [ obj1, obj2 ] = [...objetos];
-    expect(obj1).toEqual(obj2);
+    const [obj1, obj2, obj3] = [...objetos];
+    expect(obj1).toEqual(obj3);
   })
 
 });
