29c8cfec9a6ddce9318c3c95d8145dec83f27f28
unamednada <dias.gustavo.dias@gmail.com>
2021-08-17 10:00:14 -0300

    Pass frist test myremovewithoutcopy. success

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-01-primeiros-passos-no-jest/parte-1/myRemoveWithoutCopy.test.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-01-primeiros-passos-no-jest/parte-1/myRemoveWithoutCopy.test.js
@@ -1,1 +1,9 @@
 const myRemoveWithoutCopy = require('./myRemoveWithoutCopy.js');
+
+describe('A função myRemoveWithoutCopy(arr, item) retorna o array original sem o item', () => {
+  
+  it('A chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) retorna o array esperado', () => {
+    expect([1, 2, 4]).toEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3));
+  });
+
+})
