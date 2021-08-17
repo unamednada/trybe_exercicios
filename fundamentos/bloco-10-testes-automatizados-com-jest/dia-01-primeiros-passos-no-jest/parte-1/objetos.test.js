81e3b654d1582a81839d2127aff84613d3a69e80
unamednada <dias.gustavo.dias@gmail.com>
2021-08-17 13:38:13 -0300

    Refactor test nottoequal

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-01-primeiros-passos-no-jest/parte-1/objetos.test.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-01-primeiros-passos-no-jest/parte-1/objetos.test.js
@@ -1,10 +1,14 @@
 const objetos = require('./objetos');
 
 describe('Compare 2 objetos para verificar se são idênticos ou não', () => {
+  const [obj1, obj2, obj3] = [...objetos];
 
   it('Comparar 2 objetos não idênticos', () => {
-    const [obj1, obj2, obj3] = [...objetos];
-    expect(obj1).toEqual(obj3);
+    expect(obj2).not.toEqual(obj3);
+  });
+
+  it('Comparar as chaves de 2 objetos para verificar se são idênticos', () => {
+    expect(obj1).objectContaining({...obj2});
   })
 
 });
