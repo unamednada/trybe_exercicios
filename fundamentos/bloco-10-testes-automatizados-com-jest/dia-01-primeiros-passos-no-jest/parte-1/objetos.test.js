739d3f0774ce1a9f88b424f93ce159baebe04522
unamednada <dias.gustavo.dias@gmail.com>
2021-08-17 13:33:01 -0300

    Create test to fail checking if obj1 toequal obj2

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-01-primeiros-passos-no-jest/parte-1/objetos.test.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-01-primeiros-passos-no-jest/parte-1/objetos.test.js
@@ -1,2 +1,10 @@
 const objetos = require('./objetos');
 
+describe('Compare 2 objetos para verificar se são idênticos ou não', () => {
+
+  it('Comparar 2 objetos não idênticos', () => {
+    const [ obj1, obj2 ] = [...objetos];
+    expect(obj1).toEqual(obj2);
+  })
+
+});
