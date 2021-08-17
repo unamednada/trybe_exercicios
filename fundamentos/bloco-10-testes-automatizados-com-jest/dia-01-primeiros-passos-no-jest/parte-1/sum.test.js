4bc03a02b6cd80aca07c3a1a373e487626fc2fc5
unamednada <dias.gustavo.dias@gmail.com>
2021-08-17 09:32:38 -0300

    Add first test. Success

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-01-primeiros-passos-no-jest/parte-1/sum.test.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-01-primeiros-passos-no-jest/parte-1/sum.test.js
@@ -1,2 +1,9 @@
 const sum = require('./sum.js');
 
+describe('A função sum(a, b) retorna a soma do parâmetro a com o b', () => {
+
+  it('O retorno de sum(4, 5) é 9', () => {
+    expect(9).toBe(sum(4, 5));
+  });
+
+});
