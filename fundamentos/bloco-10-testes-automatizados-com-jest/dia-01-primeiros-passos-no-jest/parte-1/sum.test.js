370cee170d49a68ad16b5db947a15c21d89b386b
unamednada <dias.gustavo.dias@gmail.com>
2021-08-17 09:35:40 -0300

    Tests 2 and 3 success

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-01-primeiros-passos-no-jest/parte-1/sum.test.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-01-primeiros-passos-no-jest/parte-1/sum.test.js
@@ -6,4 +6,12 @@ describe('A função sum(a, b) retorna a soma do parâmetro a com o b', () => {
     expect(9).toBe(sum(4, 5));
   });
 
+  it('O retorno de sum(0, 0) é 0', () => {
+    expect(sum(0, 0)).not.toBe(true);
+  });
+
+  it('A função sum() lança erro quando um dos parâmetros é uma string', () => {
+    expect(() => { sum(4, '5') }).toThrow();
+  });
+
 });
