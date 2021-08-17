32969839a1f54c53c7998fd3376b6ab4802495a7
unamednada <dias.gustavo.dias@gmail.com>
2021-08-17 10:03:01 -0300

    Pass 3rd test myremovewithoutcopy success

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-01-primeiros-passos-no-jest/parte-1/myRemoveWithoutCopy.test.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-01-primeiros-passos-no-jest/parte-1/myRemoveWithoutCopy.test.js
@@ -8,5 +8,11 @@ describe('A função myRemoveWithoutCopy(arr, item) retorna o array original sem
 
   it('A chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) NÃO retorna o array [1, 2, 3, 4]', () => {
     expect(myRemoveWithoutCopy([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  });
+
+  it('O array passado por parâmetro sofre alterações após a chamada de myRemoveWithoutCopy', () => {
+    const arr = [1, 2, 3, 4];
+    myRemoveWithoutCopy(arr, 3);
+    expect(arr).not.toEqual([1, 2, 3, 4]);
   })
 })
