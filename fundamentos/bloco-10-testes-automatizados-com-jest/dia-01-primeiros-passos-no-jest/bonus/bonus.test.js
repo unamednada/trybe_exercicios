ddcd97f82b92d82f03d423b4144cb8ab6f6d41af
unamednada <dias.gustavo.dias@gmail.com>
2021-08-18 10:14:34 -0300

    Create and fail test for random params array searchemployee

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-01-primeiros-passos-no-jest/bonus/bonus.test.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-01-primeiros-passos-no-jest/bonus/bonus.test.js
@@ -8,7 +8,10 @@ describe('Verifica se a função searchEmployee se comporta como esperado', () =
 
   it('Verifica se o retorno de searchEmployee é uma string', () => {
     expect(typeof searchEmployee()).toBe('string');
-    expect(typeof searchEmployee('0000-0')).toBe('string');
+    
+    const params = [['0000-0'], ['0000-0', 'firstName'], ['8579-6'], ['8579-6', 'lastName'], ['9852-2-2', 'specialities'], ['4456-4', 'fullName']];
+
+    expect(params.map((param) => typeof searchEmployee(...param))).toEqual('string '.repeat(params.length).split(' ').slice(0, -1));
   })
 
 })
