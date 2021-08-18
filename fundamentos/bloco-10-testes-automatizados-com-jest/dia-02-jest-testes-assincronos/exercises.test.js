b4f149efce5eae324b6566d957477e89090ed5d3
unamednada <dias.gustavo.dias@gmail.com>
2021-08-18 16:18:21 -0300

    Test if uppercase is a function. success

---
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-02-jest-testes-assincronos/exercises.test.js
@@ -0,0 +1,8 @@
+const upperCase = require('./exercises.js');
+
+describe('Test if upperCase works as expected', () => {
+
+  it('Test if upperCase is a function', () => {
+    expect(typeof upperCase).toBe('function');
+  })
+})
