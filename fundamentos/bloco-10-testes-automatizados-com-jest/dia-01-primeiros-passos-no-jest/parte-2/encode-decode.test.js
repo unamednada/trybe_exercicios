0ca4ace6a7eec64ea4c732bdcdca50136745f746
unamednada <dias.gustavo.dias@gmail.com>
2021-08-17 22:21:34 -0300

    Create test for typeof encode and decode success

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-01-primeiros-passos-no-jest/parte-2/encode-decode.test.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-01-primeiros-passos-no-jest/parte-2/encode-decode.test.js
@@ -2,12 +2,16 @@ const { encode, decode } = require('./encode-decode.js');
 
 describe('Verifica se encode se comporta como esperado', () => {
 
-
+  it('Verifica se encode é uma função', () => {
+    expect('function').toBe(typeof encode);
+  });
 
 })
 
-describe('Verifica se encode se comporta como esperado', () => {
+describe('Verifica se decode se comporta como esperado', () => {
 
-
+  it('Verifica se decode é uma função', () => {
+    expect('function').toBe(typeof decode);
+  });
 
 })
