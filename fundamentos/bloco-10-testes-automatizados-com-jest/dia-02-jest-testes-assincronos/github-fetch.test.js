b942555446a329003a9bfca5c4f1fca90b9412eb
unamednada <dias.gustavo.dias@gmail.com>
2021-08-18 19:36:54 -0300

    Create and pass test with correct url

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-02-jest-testes-assincronos/github-fetch.test.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-02-jest-testes-assincronos/github-fetch.test.js
@@ -15,7 +15,12 @@ describe('Test if getRepos returns desired list of repos', () => {
       const response = await getRepos('https://www.google.com');
     } catch (error) {
       console.log(error);
-    }
-    
-  })
+    };
+  });
+
+  it('with the correct URL', async () => {
+    const response = await getRepos('https://api.github.com/orgs/tryber/repos');
+    expect(response).toEqual(expect.arrayContaining(['sd-01-week4-5-project-todo-list']));
+  });
+
 });
