4c750161d6336e69c93ffa6eddd7c01615557440
unamednada <dias.gustavo.dias@gmail.com>
2021-08-17 22:19:49 -0300

    Create boilerplate describe for encode and decode

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-01-primeiros-passos-no-jest/parte-2/encode-decode.test.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-01-primeiros-passos-no-jest/parte-2/encode-decode.test.js
@@ -1,1 +1,13 @@
 const { encode, decode } = require('./encode-decode.js');
+
+describe('Verifica se encode se comporta como esperado', () => {
+
+
+
+})
+
+describe('Verifica se encode se comporta como esperado', () => {
+
+
+
+})
