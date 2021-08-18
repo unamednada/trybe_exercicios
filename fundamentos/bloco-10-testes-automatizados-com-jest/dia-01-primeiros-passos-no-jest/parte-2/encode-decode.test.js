87cee2a74cc54b2a30a4e630623ffff8bb428193
unamednada <dias.gustavo.dias@gmail.com>
2021-08-17 22:24:17 -0300

    Test encode for a, e, i, o, u success

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-01-primeiros-passos-no-jest/parte-2/encode-decode.test.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-01-primeiros-passos-no-jest/parte-2/encode-decode.test.js
@@ -6,6 +6,13 @@ describe('Verifica se encode se comporta como esperado', () => {
     expect('function').toBe(typeof encode);
   });
 
+  it('Verifica se as vogais a, e, i, o, u, são convertidas em 1, 2, 3, 4, 5, respectivamente', () => {
+    const vogais = ['a', 'e', 'i', 'o', 'u'];
+    const numbers = ['1', '2', '3', '4', '5'];
+    vogais.forEach((vogal, index) => {
+      expect(encode(vogal)).toBe(numbers[index]);
+    })
+  })
 })
 
 describe('Verifica se decode se comporta como esperado', () => {
