31072502e9ec1456070017992ad67b46e489c5cd
unamednada <dias.gustavo.dias@gmail.com>
2021-08-18 00:04:09 -0300

    Refactor myfizzbuzz pass all tests

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-01-primeiros-passos-no-jest/parte-1/myFizzBuzz.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-01-primeiros-passos-no-jest/parte-1/myFizzBuzz.js
@@ -1,9 +1,11 @@
-function myFizzBuzz(num) {
-  if (typeof num !== 'number') return false;
-  if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
-  if (num % 3 === 0) return 'fizz';
-  if (num % 5 === 0) return 'buzz';
-  return num;
-}
+// function myFizzBuzz(num) {
+//   if (typeof num !== 'number') return false;
+//   if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+//   if (num % 3 === 0) return 'fizz';
+//   if (num % 5 === 0) return 'buzz';
+//   return num;
+// }
+
+const myFizzBuzz = (num) => typeof num !== 'number' ? false : num % 3 !== 0 && num % 5 !== 0 ? num : `${num % 3 === 0 ? 'fizz' : ''}${num % 5 === 0 ? 'buzz' : ''}`;
 
 module.exports = myFizzBuzz;
