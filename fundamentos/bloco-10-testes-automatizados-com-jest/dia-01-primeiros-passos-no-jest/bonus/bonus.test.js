8723667898689b2d286a43dc33fef59e68230d80
unamednada <dias.gustavo.dias@gmail.com>
2021-08-18 10:24:36 -0300

    Create test for invalid parameter searchemployee

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-01-primeiros-passos-no-jest/bonus/bonus.test.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-01-primeiros-passos-no-jest/bonus/bonus.test.js
@@ -4,7 +4,7 @@ describe('Verifica se a função searchEmployee se comporta como esperado', () =
 
   it('Verifica se searchEmployee é uma função', () => {
     expect(typeof searchEmployee).toBe('function');
-  })
+  });
 
   it('Verifica se o retorno de searchEmployee é uma string', () => {
     expect(typeof searchEmployee()).toBe('string');
@@ -12,6 +12,10 @@ describe('Verifica se a função searchEmployee se comporta como esperado', () =
     const params = [['0000-0'], ['0000-0', 'firstName'], ['8579-6'], ['8579-6', 'lastName'], ['9852-2-2', 'specialities'], ['4456-4', 'fullName']];
 
     expect(params.map((param) => typeof searchEmployee(...param))).toEqual('string '.repeat(params.length).split(' ').slice(0, -1));
-  })
+  });
+
+  it('Verifica se searchEmployee retorna as strings corretas para parâmetros inválidos', () => {
+    expect(searchEmployee('0000-0')).toBe('ID não identificada');
+  });
 
 })
