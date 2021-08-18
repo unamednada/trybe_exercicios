a01d6089bf96303928d27bcd45e544f9fed5e08b
unamednada <dias.gustavo.dias@gmail.com>
2021-08-17 23:56:32 -0300

    Declare nums array and refactor decode following encode's model

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-01-primeiros-passos-no-jest/parte-2/encode-decode.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-01-primeiros-passos-no-jest/parte-2/encode-decode.js
@@ -1,22 +1,9 @@
 const vowels = ['a', 'e', 'i', 'o', 'u'];
+const numbers = ['1', '2', '3', '4', '5'];
 
 const encode = (string) => string.split('').reduce((str, char) => vowels.includes(char) ? `${str}${vowels.indexOf(char) + 1}` : `${str}${char}`, '');
 
-function decode(code) {
-  let string = '';
-  let keys = ['1', '2', '3', '4', '5'];
-  let vowels = ['a', 'e', 'i', 'o', 'u'];
-
-  for (let i = 0; i < code.length; i += 1) {
-    if (keys.includes(code[i])) {
-      string += vowels[keys.indexOf(code[i])];
-    } else {
-      string += code[i];
-    }
-  }
-
-  return string;
-}
+const decode = (string) => string.split('').reduce((str, num) => numbers.includes(num) ? `${str}${vowels[num - 1]}` : `${str}${num}`, '');
 
 module.exports = {
   encode,
