01c68bc8738fa175551c8ef97b36cd46b43b9141
unamednada <dias.gustavo.dias@gmail.com>
2021-08-19 23:22:29 -0300

    Create 3 functions for exercise 4

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-03-jest-simulando-comportamentos/random.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-03-jest-simulando-comportamentos/random.js
@@ -1,5 +1,7 @@
 const randomToTest = () => Math.floor(Math.random() * 101);
 
-console.log(randomToTest());
+const toUpper = (str) => str.toUpperCase();
+const firstChar = (str) => str[0];
+const concat = (str1, str2) => str1 + str2; 
 
 module.exports = { randomToTest };
