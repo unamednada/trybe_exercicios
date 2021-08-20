2bea744e68ea471d62700cc454b7e687882f213d
unamednada <dias.gustavo.dias@gmail.com>
2021-08-20 13:47:17 -0300

    Succesfully tests fetch using github api

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-03-jest-simulando-comportamentos/dog-pics.test.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-03-jest-simulando-comportamentos/dog-pics.test.js
@@ -1,15 +1,24 @@
 const dog = require('./dog-pics.js');
 
 describe('Tesa se a função fetch', () => {
+  const url = 'https://api.github.com';
 
   it('foi importada corretamente', () => {
     expect(typeof dog.fetch).toBe('function');
   })
 
   it('retorna uma promise', async () => {
-    const url = 'https://api.github.com';
     const response = await dog.fetch(url);
 
-    expect(typeof response).toBe('object');
+    expect(response).resolves;
+  })
+
+  it('contém um json em seu retorno', async () => {
+    const params = '/users/unamednada';
+    const endpoint = url + params;
+    const response = await dog.fetch(endpoint);
+    const jsonResponse = await response.json();
+
+    expect(jsonResponse).toEqual(expect.objectContaining({ 'name': 'Gustavo Dias Guimarães Barbosa'}));
   })
 })
