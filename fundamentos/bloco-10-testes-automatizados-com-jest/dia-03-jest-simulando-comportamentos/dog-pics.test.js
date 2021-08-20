edf03660c76826c485e20e0f9eca6f3dc442d16d
unamednada <dias.gustavo.dias@gmail.com>
2021-08-20 13:36:46 -0300

    Create and fail test for github api return

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-03-jest-simulando-comportamentos/dog-pics.test.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-03-jest-simulando-comportamentos/dog-pics.test.js
@@ -5,4 +5,9 @@ describe('Tesa se a função fetch', () => {
   it('foi importada corretamente', () => {
     expect(typeof dog.fetch).toBe('function');
   })
+
+  it('retorna uma promise', () => {
+    const url = 'https://api.github.com';
+    expect(dog.fetch(url)).toBe('');
+  })
 })
