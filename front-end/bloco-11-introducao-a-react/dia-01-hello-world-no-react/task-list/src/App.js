e0b0e003509ee78c0a807e877b7cc9b07c88aca0
unamednada <dias.gustavo.dias@gmail.com>
2021-08-30 15:45:54 -0300

    Create Task and make it use map to render list of tasks

--- front-end/bloco-11-introducao-a-react/dia-01-hello-world-no-react/task-list/src/App.js
+++ front-end/bloco-11-introducao-a-react/dia-01-hello-world-no-react/task-list/src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+const Task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+
+const tasks = ['Do laundry', 'Pick up groceries', 'Book holiday', 'Buy Jeans'];
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    tasks.map((task) => {
+      return Task(task);
+    })
   );
 }
 
