19f11c279544e42a7f187e87ade7d9a2d92a6190
unamednada <dias.gustavo.dias@gmail.com>
2021-08-31 16:54:33 -0300

    Create pokemon.css and rules for poke-div

--- front-end/bloco-11-introducao-a-react/dia-02-componentes-react/my-pokedex/src/components/Pokemon.js
+++ front-end/bloco-11-introducao-a-react/dia-02-componentes-react/my-pokedex/src/components/Pokemon.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import './Pokemon.css';
 
 export class Pokemon extends React.Component {
 
   render() {
 
     return (
-      <div className="poke-div">
-        <h1>{this.props.name}</h1>
-        <p>{this.props.type}</p>
-        <p>Average weight: {this.props.averageWeight.value} {this.props.averageWeight.measurementUnit}</p>
+      <section className="poke-div">
+        <div>
+          <h1>{this.props.name}</h1>
+          <p>{this.props.type}</p>
+          <p>Average weight: {this.props.averageWeight.value} {this.props.averageWeight.measurementUnit}</p>
+        </div>
         <img src={this.props.image}alt={this.props.name}></img>
-      </div>
+      </section>
     )
   }
 
