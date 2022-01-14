ffe8915cc8cfb329dc6e8dbb7c1fa07ac878fbf3
unamednada <dias.gustavo.dias@gmail.com>
2022-01-14 18:54:33 -0300

    Create velocidade and sorteio and scripts for them

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-nodejs-um-motor-javascript/my-scripts/imc.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-nodejs-um-motor-javascript/my-scripts/imc.js
@@ -1,1 +1,8 @@
-console.log(5);
+const readLine = require('readline-sync');
+
+const weight = readLine.questionFloat('Informe seu peso, em kg:\n');
+const height = readLine.questionFloat('Informe sua altura, em m:\n');
+
+const imc = (weight / (height * height)).toFixed(2);
+
+console.log(`Seu IMC é: ${imc}`);
