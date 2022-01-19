abebb677d36aaa44a6ef4ae8f2cbbba41fa7fc84
unamednada <dias.gustavo.dias@gmail.com>
2022-01-19 18:03:34 -0300

    Trial with express-rescue. fail

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-05-express-middlewares/user/userRouter.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-05-express-middlewares/user/userRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const rescue = require('express-rescue');
 
 const validateLogin = require('./validateLogin');
 const validateUsername = require('./validateUsername');
@@ -8,9 +9,23 @@ router.use(validateLogin);
 const generateToken = require('./generateToken');
 const token = generateToken();
 
-router.post('/login', (req, res) => {
-  res.status(200).json({ token });
-});
+const loginError = (err, req, res, next) => {
+  if (err.code && err.status) {
+    return res.status(err.status).json({ message: err.message, code: err.code });
+  }
+
+  return res.status(400).json({ message: 'email or password is incorrect' });
+}
+
+router.post('/login',[
+  rescue((req, res) => {
+    res.status(200).json({ token });
+  }),
+  (err, req, res, next) => {
+    return next(err);
+  },
+  loginError
+]);
 
 router.use(validateUsername);
 
