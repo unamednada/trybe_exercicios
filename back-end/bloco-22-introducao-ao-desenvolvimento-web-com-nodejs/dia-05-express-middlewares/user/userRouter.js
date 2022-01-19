27dfec047c47c8988bda3158aefca1467b376ef4
unamednada <dias.gustavo.dias@gmail.com>
2022-01-19 17:09:30 -0300

    Create login router succesfully

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-05-express-middlewares/user/userRouter.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-05-express-middlewares/user/userRouter.js
@@ -4,6 +4,14 @@ const router = express.Router();
 const validateLogin = require('./validateLogin');
 const validateUsername = require('./validateUsername');
 router.use(validateLogin);
+
+const generateToken = require('./generateToken');
+const token = generateToken();
+
+router.post('/login', (req, res) => {
+  res.status(200).json({ token });
+});
+
 router.use(validateUsername);
 
 router.post('/register', (req, res) => {
