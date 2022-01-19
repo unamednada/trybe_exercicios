a8472a5a3a877c92b65d35cc18625f99cc4601ab
unamednada <dias.gustavo.dias@gmail.com>
2022-01-19 14:23:39 -0300

    Minor alterations on app.post simpsons. problem was in post request == instead of = for params

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-express-http-com-nodejs/simpsons/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-express-http-com-nodejs/simpsons/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 const app = express();
 app.use(bodyParser.json());
-const simpsons = require('./simpsons.json');
+
+let simpsons;
+fs.readFile('./simpsons.json', 'utf-8')
+  .then(simpsonsJson => {
+    simpsons = JSON.parse(simpsonsJson);
+  });
 
 app.get('/simpsons', (req, res) => {
   res.json(simpsons);
@@ -13,6 +19,17 @@ app.get('/simpsons/:id', (req, res) => {
   const simpson = simpsons.find(simpson => simpson.id === id);
   if (!simpson) return res.status(404).json({ message: 'simpson not found' });
   res.json(simpson);
+});
+
+app.post('/simpsons', (req, res) => {
+  const { id, name } = req.body;
+  const simpsonExists = simpsons.find(simpson => simpson.id === id);
+  if (simpsonExists) return res.status(409).json({ message: 'id already exists' });
+  simpsons.push({ id, name });
+  fs.writeFile('./simpsons.json', JSON.stringify(simpsons))
+    .then(() => {
+      res.status(204).end();
+    });
 });
 
 app.listen(3001, () => {
