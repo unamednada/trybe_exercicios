7e2374a95e0e31c81012b115879ad7b7c778ba2e
unamednada <dias.gustavo.dias@gmail.com>
2022-01-19 12:39:09 -0300

    Create get simpsons id with if success

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-express-http-com-nodejs/simpsons/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-express-http-com-nodejs/simpsons/index.js
@@ -8,6 +8,13 @@ app.get('/simpsons', (req, res) => {
   res.json(simpsons);
 });
 
+app.get('/simpsons/:id', (req, res) => {
+  const { id } = req.params;
+  const simpson = simpsons.find(simpson => simpson.id === id);
+  if (!simpson) return res.status(404).json({ message: 'simpson not found' });
+  res.json(simpson);
+});
+
 app.listen(3001, () => {
   console.log('App listening on PORT 3001');
 });
