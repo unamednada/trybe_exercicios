27fc94456ea11fcb2575b10fe1bb16a1ebfed6e7
unamednada <dias.gustavo.dias@gmail.com>
2022-01-18 22:10:47 -0300

    Create POST in express-exercises

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-express-http-com-nodejs/express-exercises/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-express-http-com-nodejs/express-exercises/index.js
@@ -7,6 +7,12 @@ const pong = { message: 'pong '};
 
 app.get('/ping', (req, res) => res.json(pong));
 
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+  const hello = { message: `Hello, ${name}` };
+  res.json(hello);
+});
+
 app.listen(3001, () => {
   console.log('App listening on PORT 3001');
 });
