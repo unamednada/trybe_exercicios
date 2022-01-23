2c922ce8c77e1ed45d8102cee04ec914c0eef91a
unamednada <dias.gustavo.dias@gmail.com>
2022-01-23 19:28:44 -0300

    Create productmodel and database rest_exercicios on localhost

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-03-noseja-camada-de-servico-e-arquitetura-rest-e-restful/refactoring/models/connection.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-03-noseja-camada-de-servico-e-arquitetura-rest-e-restful/refactoring/models/connection.js
@@ -1,6 +1,7 @@
-const mysql = require('mysql2/promise');
-
+require('dotenv').config();
 const { MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB_NAME } = process.env;
+
+const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
   host: MYSQL_HOST,
