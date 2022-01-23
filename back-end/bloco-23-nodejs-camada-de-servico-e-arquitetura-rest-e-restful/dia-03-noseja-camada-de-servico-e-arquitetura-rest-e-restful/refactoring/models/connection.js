229f858aedbbd61b03a6d21ed820f343b9c7d91c
unamednada <dias.gustavo.dias@gmail.com>
2022-01-23 19:25:59 -0300

    Create models connection

---
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-03-noseja-camada-de-servico-e-arquitetura-rest-e-restful/refactoring/models/connection.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql2/promise');
+
+const { MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB_NAME } = process.env;
+
+const connection = mysql.createPool({
+  host: MYSQL_HOST,
+  user: MYSQL_USER,
+  password: MYSQL_PASSWORD,
+  database: MYSQL_DB_NAME,
+});
+
+module.exports = connection;
