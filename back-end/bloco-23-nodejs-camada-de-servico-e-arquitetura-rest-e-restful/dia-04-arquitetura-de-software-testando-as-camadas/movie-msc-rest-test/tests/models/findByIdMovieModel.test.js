46c06d8308ebece53aaffee40ccd0d7090e8687d
unamednada <dias.gustavo.dias@gmail.com>
2022-01-24 19:19:17 -0300

    Adjust test resolves stub and return model. test ok with stub

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-04-arquitetura-de-software-testando-as-camadas/movie-msc-rest-test/tests/models/findByIdMovieModel.test.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-04-arquitetura-de-software-testando-as-camadas/movie-msc-rest-test/tests/models/findByIdMovieModel.test.js
@@ -2,32 +2,43 @@ const sinon = require('sinon');
 const { expect } = require('chai');
 
 const MovieModel = require('../../models/movieModel');
+const connection = require('../../models/connection');
 
 describe('Find movie by id in DB', () => {
-  describe('when the id exists in DB', () => {
-    const ID = 2;
-    const returnMovie = {
-      title: 'Avatar',
-      directedBy: 'James Cameron',
-      releaseYear: 2009,
-    };
+  const VALID_ID = 2;
+  const returnMovie = {
+    title: 'Avatar',
+    directed_by: 'James Cameron',
+    release_year: 2009,
+  };
 
+  before(async () => {
+    const SELECT_SUCCESS = [[returnMovie]];
+
+    sinon.stub(connection, 'execute').resolves(SELECT_SUCCESS);
+  });
+
+  after(async () => {
+    connection.execute.restore();
+  });
+
+  describe('when the id exists in DB', () => {
     it('returns an object', async () => {
-      const response = await MovieModel.findById(ID);
+      const response = await MovieModel.findById(VALID_ID);
 
       expect(response).to.be.a('object');
     });
 
     it('contains the correct data of inserted movie', async () => {
-      const response = await MovieModel.findById(ID);
+      const response = await MovieModel.findById(VALID_ID);
 
       expect(response).to.have.property('title');
       expect(response).to.have.property('directedBy');
       expect(response).to.have.property('releaseYear');
       const { title, directedBy, releaseYear } = response;
       expect(title).to.be.eq(returnMovie.title);
-      expect(directedBy).to.be.eq(returnMovie.directedBy);
-      expect(releaseYear).to.be.eq(returnMovie.releaseYear);
+      expect(directedBy).to.be.eq(returnMovie.directed_by);
+      expect(releaseYear).to.be.eq(returnMovie.release_year);
     });
   })
 });
