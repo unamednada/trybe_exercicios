b4d059ae84184a3164a053fe3c177adc27f23113
unamednada <dias.gustavo.dias@gmail.com>
2022-01-24 16:57:15 -0300

    Create .env in dia-01 bloco 23

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-01-arquitetura-de-software-camada-de-model/user/models/connection.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-01-arquitetura-de-software-camada-de-model/user/models/connection.js
@@ -1,13 +1,17 @@
-    // models/connection.js
+require('dotenv').config();
+const {
+  MYSQL_HOST,
+  MYSQL_USER,
+  MYSQL_PASSWORD,
+  MYSQL_DB_NAME,
+} = process.env;
+const mysql = require('mysql2/promise');
 
-    const mysql = require('mysql2/promise');
+const connection = mysql.createPool({
+    host: 'localhost',
+    user: 'unamednada',
+    password: '36856914',
+    database: 'trybe_exercises_user'
+});
 
-    const connection = mysql.createPool({
-        host: 'localhost',
-        user: 'unamednada',
-        password: '36856914',
-        database: 'trybe_exercises_user'
-    });
-
-    module.exports = connection;
-    
+module.exports = connection;
