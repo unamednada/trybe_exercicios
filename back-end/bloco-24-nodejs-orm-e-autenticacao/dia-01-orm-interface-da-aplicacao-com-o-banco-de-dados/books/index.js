a1df452c2dea701ed8b4707bef6cc420f168bab5
unamednada <dias.gustavo.dias@gmail.com>
2022-02-01 14:07:18 -0300

    Configure sequelize and create migration create-books. need create db first

--- back-end/bloco-24-nodejs-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/books/index.js
+++ back-end/bloco-24-nodejs-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/books/index.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-
+//
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => { console.log(`Listening on port ${PORT}`); });
