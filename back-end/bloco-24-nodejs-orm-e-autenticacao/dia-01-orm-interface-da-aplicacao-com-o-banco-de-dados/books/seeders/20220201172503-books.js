1b42adfbce2b36202a76babb065a6585bada5483
unamednada <dias.gustavo.dias@gmail.com>
2022-02-01 14:31:33 -0300

    Fix seeder to take snake case

--- back-end/bloco-24-nodejs-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/books/seeders/20220201172503-books.js
+++ back-end/bloco-24-nodejs-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/books/seeders/20220201172503-books.js
@@ -2,23 +2,21 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    queryInterface.bulkInsert('Books', [
+      {
+        title: 'Journey to the centre of the Earth',
+        author: 'Julio Verne',
+        created_at: Sequelize.literal('CURRENT_DATE'),
+      },
+      {
+        title: 'What the bleep do we know?',
+        author: 'Betsy',
+        created_at: Sequelize.literal('CURRENT_DATE'),
+      },
+    ], {});
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    queryInterface.bulkDelete('Books', null, {});
   }
 };
