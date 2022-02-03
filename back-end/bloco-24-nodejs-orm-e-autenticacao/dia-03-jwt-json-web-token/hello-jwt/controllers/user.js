c0e00654a99d2b2bb9db951fb00aa7208bfe9af4
unamednada <dias.gustavo.dias@gmail.com>
2022-02-03 09:58:06 -0300

    Create topsecret controller draft. need work

--- back-end/bloco-24-nodejs-orm-e-autenticacao/dia-03-jwt-json-web-token/hello-jwt/controllers/user.js
+++ back-end/bloco-24-nodejs-orm-e-autenticacao/dia-03-jwt-json-web-token/hello-jwt/controllers/user.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 
 const { SECRET } = process.env;
 
-module.exports = (req, res, next) => {
+module.exports = (req, res) => {
   const { authorization } = req.headers;
   const auth = jwt.decode(authorization, SECRET);
   const { data: { username, admin } } = auth;
