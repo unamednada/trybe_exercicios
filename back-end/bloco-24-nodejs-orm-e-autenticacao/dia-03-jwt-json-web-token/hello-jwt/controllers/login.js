8ce92c7eb7c940b870b9d03ef2c049d0214998f5
unamednada <dias.gustavo.dias@gmail.com>
2022-02-03 08:39:20 -0300

    Create endpoint for login and return jwt without validation

---
+++ back-end/bloco-24-nodejs-orm-e-autenticacao/dia-03-jwt-json-web-token/hello-jwt/controllers/login.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
+const { SECRET } = process.env;
+
+module.exports = (req, res) => {
+  const { username, password } = req.body;
+
+  const jwtConfig = {
+    expiresIn: '1h',
+    algorithm: 'HS256',
+  };
+
+  const token = jwt.sign({ data: { username, admin: false } }, SECRET.replace, jwtConfig);
+
+  res.status(200).json({ token });
+};
