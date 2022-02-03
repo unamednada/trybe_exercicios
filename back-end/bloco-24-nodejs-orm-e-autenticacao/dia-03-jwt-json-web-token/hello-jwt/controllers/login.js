f0ab7d3a8329fefd2a9bea98f740756af5c24ed8
unamednada <dias.gustavo.dias@gmail.com>
2022-02-03 14:33:50 -0300

    Refactor login to return admin according do db

--- back-end/bloco-24-nodejs-orm-e-autenticacao/dia-03-jwt-json-web-token/hello-jwt/controllers/login.js
+++ back-end/bloco-24-nodejs-orm-e-autenticacao/dia-03-jwt-json-web-token/hello-jwt/controllers/login.js
@@ -1,17 +1,24 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
+const { User } = require('../models');
 
 const { SECRET } = process.env;
 
-module.exports = (req, res) => {
+module.exports = async (req, res, next) => {
   const { username, password } = req.body;
   
+  const user = await User.findOne({
+    where: { username, password },
+  });
+
+  if (!user) return next({ statusCode: 403, message: 'user or password incorrect' });
+
+  const { admin } = user;
+
   const jwtConfig = {
     expiresIn: '1h',
     algorithm: 'HS256',
   };
-
-  const admin = username === 'admin' && password === 's3nh4S3gur4???';
 
   const token = jwt.sign({ data: { username, admin } }, SECRET, jwtConfig);
 
