477d34a6fc7a91bbaf5bb309cf192df3cb278f53
unamednada <dias.gustavo.dias@gmail.com>
2022-02-03 09:50:27 -0300

    Refactor users/me to return username and admin

--- back-end/bloco-24-nodejs-orm-e-autenticacao/dia-03-jwt-json-web-token/hello-jwt/controllers/user.js
+++ back-end/bloco-24-nodejs-orm-e-autenticacao/dia-03-jwt-json-web-token/hello-jwt/controllers/user.js
@@ -1,11 +1,19 @@
+require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
+const { SECRET } = process.env;
+
 module.exports = (req, res, next) => {
-  const { Authorization } = req.headers;
-  
-  if (!Authorization) {
+  const { authorization } = req.headers;
+  console.log(req.headers);
+  if (!authorization) {
     return next({ statusCode: 401, message: 'Token not found' });
   }
-
-  res.status(200).send('Welcome!');
+  try {
+    const auth = jwt.verify(authorization, SECRET);
+    const { data: { username, admin } } = auth;
+    res.status(200).json({ username, admin });
+  } catch (err) {
+    return next({ statusCode: 401, message: err.message });
+  }
 };
