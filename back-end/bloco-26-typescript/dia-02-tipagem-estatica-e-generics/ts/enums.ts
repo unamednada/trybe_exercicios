2b5c500a910507d2d45fe0767d3d8fbe2c0f37f5
unamednada <dias.gustavo.dias@gmail.com>
2022-02-24 12:29:43 -0300

    Fix global issues and return to project. enums ok

---
+++ back-end/bloco-26-typescript/dia-02-tipagem-estatica-e-generics/ts/enums.ts
@@ -0,0 +1,23 @@
+enum Months {
+  JANUARY = 1,
+  FEBRUARY,
+  MARCH,
+  APRIL,
+  MAY,
+  JUNE,
+  JULY,
+  AUGUST,
+  SEPTEMBER,
+  OCTOBER,
+  NOVEMBER,
+  DECEMBER,
+};
+
+enum Seasons {
+  SPRING = 'Spring',
+  SUMMER = 'Summer',
+  AUTUMN = 'Autumn',
+  WINTER = 'Winter',
+}
+
+console.log(Seasons);
