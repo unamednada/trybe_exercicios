8796cd976b5abc7d39607a533f444648a1e049c3
unamednada <dias.gustavo.dias@gmail.com>
2022-03-04 15:51:31 -0300

    Prepare folders for utils and add get route to users. query OK

--- back-end/bloco-26-typescript/dia-03-express-com-typescript/crud-users/index.ts
+++ back-end/bloco-26-typescript/dia-03-express-com-typescript/crud-users/index.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from 'express';
+import UsersRoutes from './routes/users';
 
 const app = express();
 
@@ -7,6 +8,8 @@ const PORT = 8080;
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript');
 });
+
+app.use(UsersRoutes);
 
 app.listen(PORT, () => {
   console.log('Server is running at http://localhost:' + PORT);
--- back-end/bloco-26-typescript/dia-03-express-com-typescript/crud-users/routes/users.ts
+++ back-end/bloco-26-typescript/dia-03-express-com-typescript/crud-users/routes/users.ts
@@ -1,9 +1,15 @@
 import { Router, Request, Response } from 'express';
+import fs from 'fs/promises';
+import User from '../interfaces/User';
 
 const router = Router();
 
-router.get('/users', (req: Request, res: Response) => {
+router.get('/users', async (req: Request, res: Response) => {
+  const data = await fs.readFile('./data/users.json', 'utf-8');
 
+  const users: User[] = JSON.parse(data);
+
+  return res.status(200).json(users);
 });
 
 export default router;
