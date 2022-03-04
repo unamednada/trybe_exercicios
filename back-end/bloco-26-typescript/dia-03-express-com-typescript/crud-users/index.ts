52cfa24e1fbda0bc9957fb18a7276c2ac5a773d6
unamednada <dias.gustavo.dias@gmail.com>
2022-03-04 16:01:08 -0300

    Apply statuscode enum to users routes and add app type to index.ts

--- back-end/bloco-26-typescript/dia-03-express-com-typescript/crud-users/index.ts
+++ back-end/bloco-26-typescript/dia-03-express-com-typescript/crud-users/index.ts
@@ -1,7 +1,7 @@
-import express, { Request, Response } from 'express';
+import express, { Application, Request, Response } from 'express';
 import UsersRoutes from './routes/users';
 
-const app = express();
+const app: Application = express();
 
 const PORT = 8080;
 
--- back-end/bloco-26-typescript/dia-03-express-com-typescript/crud-users/routes/users.ts
+++ back-end/bloco-26-typescript/dia-03-express-com-typescript/crud-users/routes/users.ts
@@ -1,11 +1,12 @@
 import { Router, Request, Response } from 'express';
 import { readUsers } from '../utils/functions';
+import StatusCode from '../utils/StatusCode';
 
 const router = Router();
 
 router.get('/users', async (req: Request, res: Response) => {
   const users = await readUsers();
-  return res.status(200).json(users);
+  return res.status(StatusCode.OK).json(users);
 });
 
 router.get('/users/:id', async (req: Request, res: Response) => {
@@ -14,9 +15,9 @@ router.get('/users/:id', async (req: Request, res: Response) => {
   const users = await readUsers();
   const user = users.find(user => user.id === id);
 
-  if (!user) return res.status(404).send('User not found');
+  if (!user) return res.status(StatusCode.NOT_FOUND).send('User not found');
 
-  return res.status(200).json(user);
+  return res.status(StatusCode.OK).json(user);
 })
 
 export default router;
