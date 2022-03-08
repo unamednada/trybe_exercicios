5a317ef88b00c8326683c5fbc8e90f331e2acf72
unamednada <dias.gustavo.dias@gmail.com>
2022-03-08 15:23:41 -0300

    Create index, install dependencies and test. success

---
+++ back-end/bloco-26-typescript/dia-03-express-com-typescript/crud-blog-posts/index.ts
@@ -0,0 +1,15 @@
+// ./index.ts
+
+import express from 'express';
+
+const app = express();
+
+const PORT = 8080;
+
+app.get('/', (req, res) => {
+    res.send('Express + TypeScript')
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
