9dfabccd9474fe47b441658ce304f8b97bdf0514
unamednada <dias.gustavo.dias@gmail.com>
2022-03-10 16:51:18 -0300

    Test and fail, fix query params get. query ok with term

--- back-end/bloco-26-typescript/dia-03-express-com-typescript/crud-blog-posts/routes/blogPosts.ts
+++ back-end/bloco-26-typescript/dia-03-express-com-typescript/crud-blog-posts/routes/blogPosts.ts
@@ -6,6 +6,20 @@ import StatusCode from '../utils/Status';
 
 const router: Router = Router();
 
+router.get('/blogposts/search', async (req: Request, res: Response) => {
+  const searchTerm = req.query.term;
+  const blogPosts = await readBlogPosts();
+  let returnBlogPosts: BlogPost[] = [];
+
+  if (typeof searchTerm === 'string') {
+    returnBlogPosts = blogPosts.filter(({ author, category, createdAt}) => author.toLowerCase().includes(searchTerm)
+    || category.toLowerCase().includes(searchTerm)
+    || createdAt?.includes(searchTerm))
+  }
+  if (returnBlogPosts.length === 0) return res.status(StatusCode.NOT_FOUND).send('No posts found');
+  return res.status(StatusCode.OK).json(returnBlogPosts);
+});
+
 router.get('/blogposts', async (req: Request, res: Response) => {
   const blogPosts = await readBlogPosts();
   return res.status(StatusCode.OK).json(blogPosts);
@@ -72,21 +86,6 @@ router.delete('/blogposts/:id', async (req: Request, res: Response) => {
 
   await writeBlogPosts(blogPosts);
   return res.status(StatusCode.NO_CONTENT).end();
-});
-
-router.get('/blogposts/search', async (req: Request, res: Response) => {
-  const searchTerm = req.query.term;
-
-  const blogPosts = await readBlogPosts();
-  let returnBlogPosts: BlogPost[] = [];
-
-  if (typeof searchTerm === 'string') {
-    returnBlogPosts = blogPosts.filter(({ author, category, createdAt}) => author.toLowerCase().includes(searchTerm)
-    || category.toLowerCase().includes(searchTerm)
-    || createdAt?.includes(searchTerm))
-  }
-  if (returnBlogPosts.length === 0) return res.status(StatusCode.NOT_FOUND).send('No posts found');
-  return res.status(StatusCode.OK).json(returnBlogPosts);
 });
 
 export default router;
