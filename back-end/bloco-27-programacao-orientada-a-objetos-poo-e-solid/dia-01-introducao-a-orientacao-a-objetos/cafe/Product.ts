04246d19e4ba6d2a1e4c2ca56c5db10465ceba02
unamednada <dias.gustavo.dias@gmail.com>
2022-03-16 10:10:51 -0300

    Create product in cafe

---
+++ back-end/bloco-27-programacao-orientada-a-objetos-poo-e-solid/dia-01-introducao-a-orientacao-a-objetos/cafe/Product.ts
@@ -0,0 +1,29 @@
+class Product {
+  private _name: string;
+  private _price: number;
+
+  constructor(name: string, price: number) {
+    this._name = name;
+    this._price = price;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  set name(value: string) {
+    if (value.length < 3) throw new Error('Name must have at least 3 characters')
+    this._name = value;
+  }
+
+  get price(): number {
+    return this._price;
+  }
+
+  set price(value: number) {
+    if (value < 0) throw new Error('Price can\'t be negative')
+    this._price = value;
+  }
+}
+
+export default Product;
