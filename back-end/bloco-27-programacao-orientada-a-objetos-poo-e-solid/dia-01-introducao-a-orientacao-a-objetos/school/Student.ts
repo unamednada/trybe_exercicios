1c88619496bd63dfce91d3320c75a11d02434ea4
unamednada <dias.gustavo.dias@gmail.com>
2022-03-16 10:02:20 -0300

    Create method for adding results

--- back-end/bloco-27-programacao-orientada-a-objetos-poo-e-solid/dia-01-introducao-a-orientacao-a-objetos/school/Student.ts
+++ back-end/bloco-27-programacao-orientada-a-objetos-poo-e-solid/dia-01-introducao-a-orientacao-a-objetos/school/Student.ts
@@ -1,13 +1,62 @@
 class Student {
-  id: number;
-  name: string;
-  examResults: number[];
-  paperResults: number[];
+  private _id: string;
+  private _name: string;
+  private _examResults: number[];
+  private _paperResults: number[];
 
-  constructor(id: number, name: string, examResults: number[], paperResults: number[]) {
-    this.id = id;
-    this.name = name;
-    this.examResults = examResults;
-    this.paperResults = paperResults;
+  constructor(id: string, name: string) {
+    this._id = id;
+    this._name = name;
+    this._examResults = [];
+    this._paperResults = [];
+  }
+
+  get id(): string {
+    return this._id;
+  }
+
+  set id(value: string) {
+    this._id = value;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  set name(value: string) {
+    if (value.length < 3) {
+      throw new Error('Name must have at least 3 characters')
+    }
+    this._name = value;
+  }
+
+  get examResults(): number[] {
+    return this._examResults;
+  }
+
+  set examResults(value: number[]) {
+    if (value.length > 4) {
+      throw new Error('Student can\'t have more than 4 exam results')
+    }
+    this._examResults = value;
+  }
+
+  get paperResults(): number[] {
+    return this._paperResults;
+  }
+
+  set paperResults(value: number[]) {
+    if (value.length > 2) {
+      throw new Error('Student can\'t have more than 2 paper results')
+    }
+    this._paperResults = value;
+  }
+
+  totalResults(): number {
+    const allResults: number[] = [...this._examResults, ...this._paperResults];
+    const total = allResults.reduce((cur, acc) => cur += acc, 0);
+    return total;
   }
 }
+
+export default Student;
---
+++ back-end/bloco-27-programacao-orientada-a-objetos-poo-e-solid/dia-01-introducao-a-orientacao-a-objetos/school/index.ts
@@ -0,0 +1,10 @@
+import Student from "./Student";
+
+const studentOne = new Student('20220001', 'Mark');
+console.log(studentOne);
+
+studentOne.examResults = [7, 8, 5, 5];
+studentOne.paperResults = [10, 8];
+
+console.log(studentOne);
+console.log(studentOne.totalResults());
