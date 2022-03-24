f673eec42c803d6ce60669dc9ec8b498e8e56feb
unamednada <dias.gustavo.dias@gmail.com>
2022-03-24 16:12:03 -0300

    Query ok. rewrite mock db

--- back-end/bloco-27-programacao-orientada-a-objetos-poo-e-solid/dia-04-solid-introducao-e-principios-s-o-e-d/plants/src/PlantsRouter.ts
+++ back-end/bloco-27-programacao-orientada-a-objetos-poo-e-solid/dia-04-solid-introducao-e-principios-s-o-e-d/plants/src/PlantsRouter.ts
@@ -5,5 +5,6 @@ const PlantsRouter = Router();
 
 PlantsRouter.get('/', PlantsController.getPlants);
 PlantsRouter.get('/:id', PlantsController.getPlant);
+PlantsRouter.delete('/:id', PlantsController.deletePlant);
 
 export default PlantsRouter;
