8a4159cc23c6a01d15dbd5e365c5b6e1ca96b923
unamednada <dias.gustavo.dias@gmail.com>
2022-03-24 13:25:40 -0300

    Refactor path to same directory. query ok

--- back-end/bloco-27-programacao-orientada-a-objetos-poo-e-solid/dia-04-solid-introducao-e-principios-s-o-e-d/plants/src/model/PlantsModel.ts
+++ back-end/bloco-27-programacao-orientada-a-objetos-poo-e-solid/dia-04-solid-introducao-e-principios-s-o-e-d/plants/src/model/PlantsModel.ts
@@ -4,7 +4,7 @@ import Connection from './Connection';
 export default class PlantsModel {
   static async getPlants(): Promise<IPlant[]> {
     const connection = Connection.getInstance();
-    await connection.connect('../../plants.json');
+    await connection.connect('./plants.json');
     const plants = await connection.getConnection();
     return plants as IPlant[];
   }
