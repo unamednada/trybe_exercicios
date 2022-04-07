5d6d3db1ab0e5e6a700a08e6ae03284d89de081d
unamednada <dias.gustavo.dias@gmail.com>
2022-04-07 15:25:53 -0300

    Create class tournamentmodel

---
+++ back-end/bloco-30-mongodb-com-nodejs-e-poo/dia-01-mongodb-e-arquitetura-msc/world-cup/src/models/TournamentModel.ts
@@ -0,0 +1,13 @@
+import { model as createModel } from 'mongoose';
+import { TournamentSchema, ITournament } from '../schemas/TournamentSchema';
+
+export default class TournamentModel {
+  constructor(
+    private tournamentModel = createModel<ITournament>('tournaments', TournamentSchema)
+  ) {}
+
+  public async getTournaments(): Promise<ITournament[]> {
+    const tournaments = this.tournamentModel.find();
+    return tournaments;
+  }
+}
