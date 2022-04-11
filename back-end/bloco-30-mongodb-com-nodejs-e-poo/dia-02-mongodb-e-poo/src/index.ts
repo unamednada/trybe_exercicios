c73b0b9a3b3123766159a8d23b91939173bc45c1
unamednada <dias.gustavo.dias@gmail.com>
2022-04-11 19:41:16 -0300

    Create routes, app and index. need query

---
+++ back-end/bloco-30-mongodb-com-nodejs-e-poo/dia-02-mongodb-e-poo/src/index.ts
@@ -0,0 +1,15 @@
+import RecordStoreController from './Controllers/RecordStore';
+import RecordStore from './Interfaces/RecordStore';
+import CustomRouter from './Routes/Router';
+import App from './server';
+
+const server = new App();
+
+const recordStoreController = new RecordStoreController();
+
+const recordStoreRouter = new CustomRouter<RecordStore>();
+recordStoreRouter.addRoute(recordStoreController);
+
+server.addRouter(recordStoreRouter.router);
+
+server.startServer();
