715dc967335a217fa7d7ea09474e6ba164e6c086
unamednada <dias.gustavo.dias@gmail.com>
2022-06-02 14:50:41 -0300

    Reorganize folders and start bonus exercises 1 and 2 test ok

---
+++ ciencia-da-computacao/bloco-33-introducao-a-python/dia-01-aprendendo-python/bonus/teste.py
@@ -0,0 +1,12 @@
+from ex_1 import menor_elemento
+from ex_2 import imprime_triangulo_recursiva
+
+
+lista = [3, 13, 40, -1, 0, 2]
+
+
+num_menor_elemento = menor_elemento(lista)
+
+
+print(num_menor_elemento)
+imprime_triangulo_recursiva(5)
