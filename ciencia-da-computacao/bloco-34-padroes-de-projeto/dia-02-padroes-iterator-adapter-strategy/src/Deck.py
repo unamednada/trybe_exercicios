c78dcf4fb7d00599735ff0b94d111baf7c2af0bf
unamednada <dias.gustavo.dias@gmail.com>
2022-06-20 19:29:53 -0300

    Add strategy to deck. reversedeck not necessary

--- ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-02-padroes-iterator-adapter-strategy/src/Deck.py
+++ ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-02-padroes-iterator-adapter-strategy/src/Deck.py
@@ -10,9 +10,10 @@ class Carta:
 
 
 class IteradorDoBaralho(Iterator):
-    def __init__(self, cartas):
+    def __init__(self, cartas, strategy):
         self._cartas = cartas
-        self._indice = 0
+        self._indice = strategy == 'reverso' and len(cartas) - 1 or 0
+        self._strategy = strategy
     
     def __next__(self):
         try:
@@ -20,7 +21,8 @@ class IteradorDoBaralho(Iterator):
         except IndexError:
             raise StopIteration()
         else:
-            self._indice += 1
+            if self._strategy == 'reverso': self._indice -= 1
+            else: self._indice += 1
             return carta
 
 
@@ -28,21 +30,28 @@ class Baralho(Iterable):
     naipes = 'copas ouros espadas paus'.split()
     valores = 'A 2 3 4 5 6 7 8 9 10 J Q K'.split()
 
-    def __init__(self):
+    def __init__(self, strategy='normal'):
         self._cartas = [
             Carta(valor, naipe)
             for naipe in self.naipes
             for valor in self.valores
         ]
+        self._strategy = strategy
 
     def __len__(self):
         return len(self._cartas)
 
     def __iter__(self):
-        return IteradorDoBaralho(self._cartas)
+        return IteradorDoBaralho(self._cartas, self._strategy)
 
 
 if __name__ == '__main__':
     baralho = Baralho()
+
+    print('Baralho normal:\n')
     for carta in baralho:
+        print(carta)
+
+    print('\nBaralho inverso:\n')
+    for carta in Baralho('reverso'):
         print(carta)
