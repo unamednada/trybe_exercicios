51ad938176b02f02e0bd4571102da40c801d4a58
unamednada <dias.gustavo.dias@gmail.com>
2022-06-28 18:25:09 -0300

    Add http request to socketserver_tcp. fail succesfuly

--- ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-01-arquitetura-de-redes-ferramentas-e-seguranca/script.sh
+++ ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-01-arquitetura-de-redes-ferramentas-e-seguranca/script.sh
@@ -8,6 +8,7 @@ curl -X POST http://127.0.0.1:5000 -H "Content-Type: application/json" -d '{"use
 #3. Faça uma chamada qualquer, utilizando o cURL, passando um header na requisição.
 curl -X GET http://127.0.0.1:5000/headers -H "Authorization: token"
 
+
 #Exercício 2
 #1. Faça uma chamada GET, utilizando o cURL, para "google.com".
 curl -X GET google.com
@@ -15,11 +16,28 @@ curl -X GET google.com
 #2. Faça uma nova chamada a "google.com", porém agora utilizando o parâmetro -L ou --location, que serve para "seguir redirecionamentos".
 curl -L google.com
 
+
 #Exercício 3
 #Agora utilizando o wget, pegue o conteúdo da página do site da Trybe, depois abra o arquivo HTML baixado em seu navegador.
 wget http://www.trybe.com.br/
+
 
 #Exercício 4
 #Agora vamos para a camada de transporte. Crie um servidor TCP usando o módulo socketserver que já vem embutido com o Python.
 python3 socketserver_tcp/server.py
 telnet localhost 8085
+
+
+#Exercício 5
+#1. Conecte no server do exercício anterior e envie informações. O server deverá imprimir as mensagens enviadas no console.
+python3 socketserver_tcp/server.py
+telnet localhost 8085
+
+#2. Pare o servidor e verifique o que aconteceu com a conexão que estava aberta com o comando telnet ou nc.
+^C
+
+
+#Exercício 6
+#Reinicie o servidor TCP e agora faça uma requisição utilizando o cURL (HTTP).
+python3 socketserver_tcp/server.py
+curl -X GET http://localhost:8085
