ec1a15ae92f5bdaa7b8806ed886abbd7215e8e14
unamednada <dias.gustavo.dias@gmail.com>
2022-06-28 16:15:39 -0300

    Add comments to script.sh

--- ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-01-arquitetura-de-redes-ferramentas-e-seguranca/script.sh
+++ ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-01-arquitetura-de-redes-ferramentas-e-seguranca/script.sh
@@ -1,3 +1,8 @@
+#1. Faça uma chamada GET, utilizando o cURL.
 curl -X GET http://127.0.0.1:5000
+
+#2. Faça uma chamada POST, utilizando o cURL, passando um JSON no body da requisição.
 curl -X POST http://127.0.0.1:5000 -H "Content-Type: application/json" -d '{"user_name":"unamednada"}'
+
+#3. Faça uma chamada qualquer, utilizando o cURL, passando um header na requisição.
 curl -X GET http://127.0.0.1:5000/headers -H "Authorization: token"
