b82787e050a587d4025df11300bef0654a6cbbdf
unamednada <dias.gustavo.dias@gmail.com>
2022-06-28 16:07:25 -0300

    Add new route for headers

--- ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-01-arquitetura-de-redes-ferramentas-e-seguranca/api/server.py
+++ ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-01-arquitetura-de-redes-ferramentas-e-seguranca/api/server.py
@@ -6,7 +6,16 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
     if request.method == 'GET':
-        return 'Hello World!'
+        return 'Hello World!', 200
     elif request.method == 'POST':
         user_name = request.get_json()['user_name']
-        return f'Hello {user_name}!'
+        return f'Hello {user_name}!', 200
+
+
+@app.route('/headers', methods=['GET'])
+def headers():
+    auth = request.headers.get('Authorization')
+
+    if auth != 'token':
+        return 'Unauthorized', 401
+    return 'Authorized', 200
