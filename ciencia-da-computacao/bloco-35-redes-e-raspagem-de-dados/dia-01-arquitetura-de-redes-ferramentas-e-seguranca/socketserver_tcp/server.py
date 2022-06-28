54ad5e43aaa846e10dafaff8934da56540739ea8
unamednada <dias.gustavo.dias@gmail.com>
2022-06-28 17:57:09 -0300

    Add conditional serving and try except for keyboard interruption

--- ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-01-arquitetura-de-redes-ferramentas-e-seguranca/socketserver_tcp/server.py
+++ ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-01-arquitetura-de-redes-ferramentas-e-seguranca/socketserver_tcp/server.py
@@ -1,4 +1,5 @@
 import socketserver
+import sys
 
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
@@ -16,14 +17,37 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
         print("{} wrote:".format(self.client_address[0]))
         print(self.data)
         # just send back the same data, but upper-cased
-        self.request.sendall(self.data.upper())
+        self.request.sendall(self.data.upper() + '\n')
+
+
+class MyTCPHandlerStream(socketserver.StreamRequestHandler):
+
+    def handle(self):
+        # self.rfile is a file-like object created by the handler;
+        # we can now use e.g. readline() instead of raw recv() calls
+        self.data = self.rfile.readline().strip()
+        print("{} wrote:".format(self.client_address[0]))
+        print(self.data)
+        # Likewise, self.wfile is a file-like object used to write back
+        # to the client
+        self.wfile.write(self.data.upper() + '\n')
 
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8085
 
-    # Create the server, binding to localhost on port 8085
-    with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
-        # Activate the server; this will keep running until you
-        # interrupt the program with Ctrl-C
+    # Create the server, binding to localhost on port 8085 according to option
+    if len(sys.argv) > 1 and sys.argv[1] == 'stream':
+        server = socketserver.ThreadingTCPServer(
+            (HOST, PORT),
+            MyTCPHandlerStream
+        )
+    else:
+        server = socketserver.ThreadingTCPServer((HOST, PORT), MyTCPHandler)
+
+    try:
         server.serve_forever()
+    except KeyboardInterrupt:
+        server.shutdown()
+        server.server_close()
+        sys.exit(0)
