dd5f8fbe59ac84eafd9f1eecb8b7c829e2c85c8a
unamednada <dias.gustavo.dias@gmail.com>
2022-06-29 10:15:31 -0300

    Remove waitress from flask_api. will create http server using http command line

--- ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-01-arquitetura-de-redes-ferramentas-e-seguranca/flask_api/server.py
+++ ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-01-arquitetura-de-redes-ferramentas-e-seguranca/flask_api/server.py
@@ -19,8 +19,3 @@ def headers():
     if auth != 'token':
         return 'Unauthorized', 401
     return 'Authorized', 200
-
-
-if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
