20ad75e1b0103a903b97f03df73c59a62c5710fb
unamednada <dias.gustavo.dias@gmail.com>
2022-07-04 15:00:03 -0300

    Create books_api after fetch

--- ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-02-raspagem-de-dados/exercises_mongodb/books_api.py
+++ ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-02-raspagem-de-dados/exercises_mongodb/books_api.py
@@ -3,6 +3,13 @@ from mongo_client import client
 category = "Open Source"
 
 db = client.catalogue
-books = db.books.find({"categories": {"$all": [category]}})
+
+
+def get_books(filter):
+    return db.books.find({"categories": {"$all": [filter]}})["title"]
+
+
+books = get_books(category)
+
 for book in books:
-    print(book["title"])
+    print(book)
