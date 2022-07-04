c7d648dddd0d5c5e12953cfcfd18e975bc41b657
unamednada <dias.gustavo.dias@gmail.com>
2022-07-04 15:13:42 -0300

    Create published books according to key

--- ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-02-raspagem-de-dados/exercises_mongodb/books_api.py
+++ ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-02-raspagem-de-dados/exercises_mongodb/books_api.py
@@ -5,13 +5,27 @@ category = "Open Source"
 db = client.catalogue
 
 
-def get_books(filter):
+def get_books_by_category(filter):
     return map(
         lambda b: b["title"], db.books.find({"categories": {"$all": [filter]}})
     )
 
 
-books = get_books(category)
+# books = get_books(category)
 
-for book in books:
-    print(book)
+# for book in books:
+#     print(book)
+
+
+def published_books_per_category():
+    pipelines = [
+        {"$match": {"status": "PUBLISH"}},
+        {"$unwind": "$categories"},
+        {"$group": {"_id": "$categories", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+    ]
+    return db.books.aggregate(pipelines)
+
+
+for category in published_books_per_category():
+    print(f"{category['_id']}: {category['count']}")
