fca2612043c7294b382841e98b5d7b4a718a6c52
unamednada <dias.gustavo.dias@gmail.com>
2022-07-04 11:06:38 -0300

    Create request in exercise 3, add headers, assert ok

--- ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-02-raspagem-de-dados/exercise2.py
+++ ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-02-raspagem-de-dados/exercise2.py
@@ -1,10 +1,11 @@
 import requests
 
 
-response = requests.get('https://api.github.com/users')
+response = requests.get("https://api.github.com/users")
 users = response.json()
 
+
 for user in users:
-    print(user['login'])
-    print(user['html_url'])
-    print('\n')
+    print(user["login"])
+    print(user["html_url"])
+    print("\n")
