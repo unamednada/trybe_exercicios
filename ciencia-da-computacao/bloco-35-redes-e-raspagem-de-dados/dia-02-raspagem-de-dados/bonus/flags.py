c74da9da93716b6a1a2a63f8ff557ce7e976dbdf
unamednada <dias.gustavo.dias@gmail.com>
2022-07-05 10:11:30 -0300

    Create scraper for flags

--- ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-02-raspagem-de-dados/bonus/flags.py
+++ ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-02-raspagem-de-dados/bonus/flags.py
@@ -7,5 +7,17 @@ response_text = requests.get(END_URL).text
 
 gallery = Selector(response_text).css(".thumb > div > a > img::attr(src)")
 
-for image in gallery.getall():
-    print(image)
+for url_image in gallery.getall():
+    filename = "flags/" + url_image.split("/")[-1]
+    try:
+        response = requests.get("https:" + url_image, timeout=3)
+        content = response.content
+        with open(filename, "wb") as f:
+            f.write(content)
+        print(f"{filename} downloaded")
+        print("-" * 20)
+    except Exception as e:
+        print(f"{filename} not downloaded")
+        print(e)
+        print("-" * 20)
+        continue
