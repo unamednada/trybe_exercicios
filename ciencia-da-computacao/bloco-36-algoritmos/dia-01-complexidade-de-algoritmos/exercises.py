8f25d60117cb65b44feb231581e1a94f31794d34
unamednada <dias.gustavo.dias@gmail.com>
2022-07-12 18:35:45 -0300

    Create better algorithm and tests. Better algorithm not better. haha

---
+++ ciencia-da-computacao/bloco-36-algoritmos/dia-01-complexidade-de-algoritmos/exercises.py
@@ -0,0 +1,47 @@
+import time
+
+
+# Original algorithm
+
+def contains_duplicate(numbers):
+    numbers.sort()
+    previous_number = 'not a number';
+    for number in numbers:
+        if(previous_number == number): return True
+        previous_number = number
+
+    return False
+
+
+def better_contains_duplicate(numbers):
+    return len(numbers) != len(set(numbers))
+
+
+if __name__ == "__main__":
+    five_thousand = list(range(5000))
+    ten_thousand = list(range(10000))
+    hundred_thousand = list(range(100000))
+
+    print('\nOriginal algorithm:')
+    start_time = time.time()
+    contains_duplicate(five_thousand)
+    print(f"Original -> 5 mil: {(time.time() - start_time)} seconds")
+    start_time = time.time()
+    contains_duplicate(ten_thousand)
+    print(f"Original -> 10 mil: {(time.time() - start_time)} seconds")
+    start_time = time.time()
+    contains_duplicate(hundred_thousand)
+    print(f"Original -> 100 mil: {(time.time() - start_time)} seconds")
+
+    print('\nBetter algorithm:')
+    start_time = time.time()
+    better_contains_duplicate(five_thousand)
+    print(f"Better -> 5 mil: {(time.time() - start_time)} seconds")
+    start_time = time.time()
+    better_contains_duplicate(ten_thousand)
+    print(f"Better -> 10 mil: {(time.time() - start_time)} seconds")
+    start_time = time.time()
+    better_contains_duplicate(hundred_thousand)
+    print(f"Better -> 100 mil: {(time.time() - start_time)} seconds")
+    
+    print('------------- END OF PROGRAM -------------')
