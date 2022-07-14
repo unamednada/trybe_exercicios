974caaf310f19bd64d1475f7deb9d37e6a57520d
unamednada <dias.gustavo.dias@gmail.com>
2022-07-14 09:51:13 -0300

    Fix count-even and create recursive version

--- ciencia-da-computacao/bloco-36-algoritmos/dia-02-recursividade-e-estrategias-para-solucao-de-problemas/exercicio1.py
+++ ciencia-da-computacao/bloco-36-algoritmos/dia-02-recursividade-e-estrategias-para-solucao-de-problemas/exercicio1.py
@@ -1,9 +1,17 @@
 def count_even(n):
-    """
-    Função que conta quantos números pares existem entre 0 e n
-    """
     count = 0
-    for i in range(n+1):
+    for i in range(1, n+1):
         if i % 2 == 0:
             count += 1
     return count
+
+
+def recursive_count_even(n):
+    if n == 0:
+        return 0
+    return int(n % 2 == 0) + recursive_count_even(n-1)
+
+
+if __name__ == '__main__':
+    print(count_even(10))
+    print(recursive_count_even(10))
