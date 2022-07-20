2ad9e40b0b0338a08327edd99225d4807b42b6c1
unamednada <dias.gustavo.dias@gmail.com>
2022-07-20 12:12:41 -0300

    Finish exercise3

--- ciencia-da-computacao/bloco-36-algoritmos/dia-03-algoritmos-de-ordenacao-e-busca/exercicio1.py
+++ ciencia-da-computacao/bloco-36-algoritmos/dia-03-algoritmos-de-ordenacao-e-busca/exercicio1.py
@@ -1,11 +1,11 @@
 animals = ["zebra", "macaco", "elefante", "arara", "javali"]
 
-def bubble_sort(list, iterations):
+def bubble_sort(arr, iterations):
     for i in range(iterations):
-        for j in range(len(list) - 1):
-            if list[j] > list[j + 1]:
-                list[j], list[j + 1] = list[j + 1], list[j]
-    return list
+        for j in range(len(arr) - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
 
 bubble_sort(animals, 2)
 print(animals)
--- ciencia-da-computacao/bloco-36-algoritmos/dia-03-algoritmos-de-ordenacao-e-busca/exercicio3.py
+++ ciencia-da-computacao/bloco-36-algoritmos/dia-03-algoritmos-de-ordenacao-e-busca/exercicio3.py
@@ -1,7 +1,53 @@
 from random import shuffle
+from cronometro import Cronometro
+
 
 ordenados = list(range(100))
-
 inversos = list(reversed(ordenados))
-
 aleatorios = shuffle(list(ordenados))
+
+
+def selection_sort(arr):
+    for i in range(len(arr)):
+        menor = i
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[menor]:
+                menor = j
+        arr[menor], arr[i] = arr[i], arr[menor]
+
+    return arr
+
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        j = i
+        while j > 0 and arr[j] < arr[j - 1]:
+            arr[j], arr[j - 1] = arr[j - 1], arr[j]
+            j -= 1
+
+    return arr
+
+
+for algorithm in (insertion_sort, selection_sort):
+    algorithm_name = algorithm.__name__
+
+    for input in (10_000, 100_000, 1_000_000):
+
+        sorted_numbers = list(range(input))
+        reversed_sorted_numbers = list(reversed(sorted_numbers))
+        random_numbers = sorted_numbers[:]  # copia dos ordenados
+        shuffle(random_numbers)  # embaralha eles
+
+        with Cronometro(f"{algorithm_name} com entrada" +
+                        f"ordenada de {input} números"):
+            algorithm(sorted_numbers)
+
+        with Cronometro(f"{algorithm_name} com entrada" +
+                        f"inversamente ordenada de {input} números"):
+            algorithm(reversed_sorted_numbers)
+
+        with Cronometro(f"{algorithm_name} com entrada" +
+                        f"aleatória de {input} números"):
+            algorithm(random_numbers)
+
+        print("*" * 50)
