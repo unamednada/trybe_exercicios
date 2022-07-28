691ccbead611caf0a3f1638fd2a3d699a613ded8
unamednada <dias.gustavo.dias@gmail.com>
2022-07-28 12:18:27 -0300

    Finish bonus exercises

--- ciencia-da-computacao/bloco-37-estrutura-de-dados-i-arrays-listas-filas-e-pilhas/dia-03-no-e-listas-encadeadas/utils.py
+++ ciencia-da-computacao/bloco-37-estrutura-de-dados-i-arrays-listas-filas-e-pilhas/dia-03-no-e-listas-encadeadas/utils.py
@@ -1,11 +1,19 @@
 from linked_list import LinkedList
+from double_linked_list import DoubleLinkedList
 
 
 def linked_list_to_linked_set(linked_list):
-    linked_set = linked_list()
+    linked_set = LinkedList()
     while linked_list:
         current_node = linked_list.pop_first()
         if linked_list.index_of(current_node.value) == -1:
             linked_set.insert_last(current_node.value)
     return linked_set
-    
+
+def double_linked_list_to_set(linked_list):
+    linked_set = DoubleLinkedList()
+    while linked_list:
+        current_node = linked_list.pop_first()
+        if linked_list.index_of(current_node.value) == -1:
+            linked_set.insert_last(current_node.value)
+    return linked_set
