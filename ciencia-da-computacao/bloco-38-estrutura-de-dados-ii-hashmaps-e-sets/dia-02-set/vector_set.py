6045c91ab6c0e5aff976f4eb6d46d1a7d6072bd7
unamednada <dias.gustavo.dias@gmail.com>
2022-08-03 09:40:24 -0300

    feat: implement all methods for vectorset

--- ciencia-da-computacao/bloco-38-estrutura-de-dados-ii-hashmaps-e-sets/dia-02-set/vector_set.py
+++ ciencia-da-computacao/bloco-38-estrutura-de-dados-ii-hashmaps-e-sets/dia-02-set/vector_set.py
@@ -1,15 +1,53 @@
 class VectorSet:
-    
+
     def __init__(self):
         self.data = [False] * 1001
         self.last_element = 0
-    
+
     def add(self, element):
         if not self.data[element]:
             self.data[element] = True
         if element > self.last_element:
             self.last_element = element
-    
+
+    def union(self, other_set):
+        union_set = VectorSet()
+        for i in range(1001):
+            if other_set.data[i] or self.data[i]:
+                self.add(i)
+
+        return union_set
+
+    def intersection(self, other_set):
+        intersection_set = VectorSet()
+        for i in range(1001):
+            if other_set.data[i] and self.data[i]:
+                intersection_set.add(i)
+
+        return intersection_set
+
+    def difference(self, other_set):
+        difference_set = VectorSet()
+        for i in range(1001):
+            if not other_set.data[i] and self.data[i]:
+                difference_set.add(i)
+
+        return difference_set
+
+    def is_subset(self, other_set):
+        for i in range(1001):
+            if self.data[i] and not other_set.data[i]:
+                return False
+
+        return True
+
+    def is_superset(self, other_set):
+        for i in range(1001):
+            if other_set.data[i] and not self.data[i]:
+                return False
+
+        return True
+
     def __str__(self):
         data_string = "{"
 
@@ -18,6 +56,9 @@ class VectorSet:
                 data_string += str(i)
                 if i < self.last_element:
                     data_string += ", "
-        
+
         data_string += "}"
         return data_string
+
+    def __contains__(self, element):
+        return self.data[element]
